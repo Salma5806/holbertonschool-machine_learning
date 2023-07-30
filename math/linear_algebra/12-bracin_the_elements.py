c1068c6f8e942a302f9226d42bbb81c06c2a8763
Salma5806 <5806@holbertonstudents.com>
2023-07-30 22:33:52 +0100

    oper

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-"""function to perform element-wise addition, subtraction, multiplication, and division using numpy"""
+"""function to perform element-wise addition, subtraction, multiplication,
+and division using numpy"""
 
 
 def np_elementwise(mat1, mat2):
     """ operate matrices """
-    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
+    return (
+        mat1 + mat2,
+        mat1 - mat2,
+        mat1 * mat2,
+        mat1 / mat2
+    )
