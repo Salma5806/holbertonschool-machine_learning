22f482731f200b8021658e5a7d34d87e3d696c07
Salma5806 <5806@holbertonstudents.com>
2023-07-30 21:33:06 +0100

    multiplication

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" function to perform matrix multiplication """
+
+def mat_mul(mat1, mat2):
+    """ return matrix multiplication """
+    if len(mat1[0]) != len(mat2):
+        return None
+    else:
+        new_mat = [[0 for i in range(len(mat2[0]))] for j in range(len(mat1))]
+        for i in range(len(mat1)):
+             for j in range(len(mat2[0])):
+                for k in range(len(mat2)):
+                    new_mat[i][j] += mat1[i][k] * mat2[k][j]
+
+        return new_mat
