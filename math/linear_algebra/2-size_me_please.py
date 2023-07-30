7d6c78360485094a3d904c99a76c4ecba2277c32
Salma5806 <5806@holbertonstudents.com>
2023-07-30 22:48:11 +0100

    shape

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -4,8 +4,7 @@
 
 def matrix_shape(matrix):
     """ return the shape of a matrix """
-    if type(matrix[0]) is not list:
+    if type(matrix[0]) != list:
         return [len(matrix)]
     else:
         return [len(matrix)] + matrix_shape(matrix[0])
-        
