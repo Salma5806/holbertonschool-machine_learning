fbbf52d9812bd1d346a008c95b600c5d47b4ba5f
Salma5806 <5806@holbertonstudents.com>
2023-07-30 19:55:39 +0100

    transpose

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" check a new matrix transposed or not """
+def matrix_transpose(matrix):
+    """ returns the transpose of a 2D matrix """
+    if type(matrix[0]) is not list:
+        return [len(matrix)]
+    else:
+        return [[row[col] for row in matrix] for col in range(len(matrix[0]))]
