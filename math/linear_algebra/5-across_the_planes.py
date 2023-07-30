11fb1c684ea6ad2cd10c5ffd1b8c149f6042c407
Salma5806 <5806@holbertonstudents.com>
2023-07-30 20:28:52 +0100

    adding two elment matrix

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" function to add two matrices element-wise """
+
+def add_matrices2D(mat1, mat2):
+    """ adding two matrices element-wise """
+    if len(mat1) != len(mat2):
+        return None
+    else:
+        return [[i + j for i, j in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
