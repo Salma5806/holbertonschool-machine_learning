d0219903d17828e3526adf6f3d9e177bd3c73003
Salma5806 <5806@holbertonstudents.com>
2023-07-30 19:44:52 +0100

    shape matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ calculates the shape of a matrix """
+
+
 def matrix_shape(matrix):
+    """ return the shape of a matrix """
     if type(matrix[0]) is not list:
         return [len(matrix)]
     else:
