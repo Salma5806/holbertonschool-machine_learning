02b8c8cfc0bb1e6a30be48fcfb27883870014ae5
Salma5806 <5806@holbertonstudents.com>
2023-07-30 20:08:27 +0100

    adding two elemnt

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""  function to add two arrays element-wise """
+
+def add_arrays(arr1, arr2):
+    """ adding two arrays element wise  """
+
+    if len(arr1) != len(arr2):
+        return None
+    else:
+        return [i + j for i, j in zip(arr1, arr2)]
