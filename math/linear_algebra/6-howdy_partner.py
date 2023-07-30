f7f284f5b30673601aa5ca12d03c2ddba0176f1e
Salma5806 <5806@holbertonstudents.com>
2023-07-30 20:55:12 +0100

    contenates array

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" function to concatenate two arrays"""
+
+def cat_arrays(arr1, arr2):
+    """ return a new list with two arrays"""
+    if type(arr1) != list and type(arr2) != list:
+        return None
+    else:
+        new_arr = arr1 + arr2
+        return new_arr
