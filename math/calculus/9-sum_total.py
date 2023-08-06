caca8bf0b3ead890de014e7ff3c468d0e16a7bc1
Salma5806 <5806@holbertonstudents.com>
2023-08-06 21:46:46 +0100

    sum

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" function that sum powers """
+
+
+def summation_i_squared(n):
+    """ n(n+1)(2n+1) / 6 """
+
+    if type(n) is not int or n < 1:
+        return None
+    else:
+        return int((n * (n + 1)) * (2 * n + 1) // 6)
+    
