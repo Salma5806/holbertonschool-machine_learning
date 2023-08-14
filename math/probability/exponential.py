c23376e6beaff8a4df41ddcfe694e73cec85a9e3
Salma5806 <5806@holbertonstudents.com>
2023-08-14 22:41:21 +0100

    exponential

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -2,17 +2,20 @@
 """represents an exponential distribution"""
 
 
-def __init__(self, data=None, lambtha=1.):
-    """ Initialize exponential """
-    if data is None:
-        if lambtha < 0 : 
-            return ValueError("lambtha must be a positive value")
+class Exponential:
+    """ represents an exponential dist """
+    def __init__(self, data=None, lambtha=1.):
+        """ Initialize exponential """
+        if data is None:
+            if lambtha < 1:
+                raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
         else:
-            self.lambtha = float(lambtha)  
-    else:
-        if type(data) is not list:
-            return TypeError("data must be a list")
-        if len(data) < 2:
-            return ValueError("data must contain multiple values")
-        else:
-            self.lambtha = sum(data) / len(data)   
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                lambtha = float(len(data) / sum(data))
+                self.lambtha = lambtha
