98c1502b1da467c2299b92abaec5293633bce110
Salma5806 <5806@holbertonstudents.com>
2023-08-14 13:38:14 +0100

    pmf

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 """ Calculate a Poisson distribution """
-import numpy as np
-import math
 
 class Poisson:
     """ Calculate a lambtha """
-    e = 2.72
 
     def __init__(self, data=None, lambtha=1.):
         """ Initialize poisson """
@@ -23,9 +20,12 @@ class Poisson:
                 self.lambtha = sum(data) / len(data)
     def pmf(self, k):
         """ calculates the value of the PMF """
+        fact = 1
         if k: 
             if type(k) is not int:
                 self.k = int(k)
             if k < 0:
                 return 0
-        return ((self.lambtha ** k) * (2.7182818285 ** -(self.lambtha))) / np.math.factorial(k)        
+            for  k in range(1, k+1):
+                fact = fact * k    
+        return ((self.lambtha ** k) * (2.7182818285 ** -(self.lambtha))) / fact        
