76e2121fdbd9f9dac4d50c391c5040bc2d04ce06
Salma5806 <5806@holbertonstudents.com>
2023-08-25 21:54:43 +0100

    private

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Script to create a Neuron"""
+
+import numpy as np
+
+
+class Neuron():
+    """ Class Neuron """
+
+    def __init__(self, nx):
+        """Initialize neuron"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def w(self):
+        """ return weight """
+        return self.__W
+    
+    def b(self):
+        """ return biais"""
+        return self.__b
+    
+    def A(self):
+        """return output"""
+        return self.__A
