9db7e2e53b83501f84bb09d553da09f66b93f0e2
Salma5806 <5806@holbertonstudents.com>
2023-08-27 23:17:56 +0100

    evaluate

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -6,23 +6,24 @@ neural network classifier
 
 
 import tensorflow.compat.v1 as tf
+import numpy as np
 
 tf.disable_eager_execution()
 
 def evaluate(X, Y, save_path):
     """Evaluates the output of a neural network"""
-    loaded_graph = tf.Graph()
-    with tf.Session(graph=loaded_graph) as sess:
-        loader = tf.train.import_meta_graph(save_path + '.meta')
-        loader.restore(sess, save_path)
-
-        x = loaded_graph.get_tensor_by_name('x:0')
-        y = loaded_graph.get_tensor_by_name('y:0')
-        prediction = loaded_graph.get_tensor_by_name('layer/Tanh:0')
-        accuracy = loaded_graph.get_tensor_by_name('Mean_1:0')
-        loss = loaded_graph.get_tensor_by_name('Mean:0')
-
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver.restore(sess, save_path)
+        
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        
+        loss = tf.get_collection("loss")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        
         feed_dict = {x: X, y: Y}
-        pred, acc, cost = sess.run([prediction, accuracy, loss], feed_dict=feed_dict)
-
+        pred, acc, cost = sess.run([y_pred, accuracy, loss], feed_dict=feed_dict)
+        
     return pred, acc, cost
