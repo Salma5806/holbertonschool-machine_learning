a659e4d47584803c26abb151315f2f899d388e2d
Salma5806 <5806@holbertonstudents.com>
2023-08-27 23:44:56 +0100

    evaluate

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -14,15 +14,16 @@ def evaluate(X, Y, save_path):
     with tf.Session() as sess:
         saver = tf.train.import_meta_graph(save_path + '.meta')
         saver.restore(sess, save_path)
-        
+
         x = tf.get_collection("x")[0]
         y = tf.get_collection("y")[0]
         y_pred = tf.get_collection("y_pred")[0]
-        
+
         loss = tf.get_collection("loss")[0]
         accuracy = tf.get_collection("accuracy")[0]
-        
+
         feed_dict = {x: X, y: Y}
-        pred, acc, cost = sess.run([y_pred, accuracy, loss], feed_dict=feed_dict)
-        
+        pred, acc, cost = sess.run([y_pred, accuracy, loss], 
+                                   feed_dict=feed_dict)
+
     return pred, acc, cost
