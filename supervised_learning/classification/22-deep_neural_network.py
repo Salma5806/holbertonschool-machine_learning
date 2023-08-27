1a22d1b7f674471adeac1e4f23ea42cf976b27b5
Salma5806 <5806@holbertonstudents.com>
2023-08-27 21:26:07 +0100

    train

--- supervised_learning/classification/22-deep_neural_network.py
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -30,3 +30,19 @@ class DeepNeuralNetwork:
                 self.__weights['W' + str(l)] = np.random.randn(layers[l - 1], layers[l - 2]) * np.sqrt(2/layers[l - 2])
             
             self.__weights['b' + str(l)] = np.zeros((layers[l - 1], 1))
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """the evaluation of the training data after iterations of training"""
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        for itr in range(iterations):
+            A, cache = self.forward_prop(X)
+            self.gradient_descent(Y, cache, alpha)
+        return (self.evaluate(X, Y))
+    
