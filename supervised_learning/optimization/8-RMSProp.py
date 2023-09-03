ba089da7f44707de2d8c7133daec0858e9792520
Salma5806 <5806@holbertonstudents.com>
2023-09-03 20:25:41 +0100

    update RMSProp

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -11,17 +11,15 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     Updates a variable using the RMSProp optimization algorithm.
 
     Args:
-        alpha: The learning rate.
-        beta2: The RMSProp weight.
-        epsilon: A small number to avoid division by zero.
-        var: A numpy.ndarray containing the variable to be updated.
-        grad: A numpy.ndarray containing the gradient of var.
-        s: The previous second moment of var.
+       loss: the loss of the network
+        alpha [float]: learning rate
+        beta2 [float]: RMSProp weight
+        epsilon [float]: small number to avoid division by zero
 
     Returns:
         The updated variable and the new moment, respectively.
     """
-    s = beta2 * s + (1 - beta2) * (grad ** 2)
-    var = var - alpha * grad / (np.sqrt(s) + epsilon)
+    op = tf.train.RMSPropOptimizer(
+        alpha, decay=beta2, epsilon=epsilon).minimize(loss)
 
-    return var, s
+    return op
