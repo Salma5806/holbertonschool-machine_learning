e23e5ba1f07abf199c6910e8ad67fcd06024a77e
Salma5806 <5806@holbertonstudents.com>
2023-09-03 20:41:46 +0100

    adam opt

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -20,14 +20,12 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
         t: The time step used for bias correction.
 
     Returns:
-        The updated variable, the new first moment, and the new second moment, respectively (all as numpy.ndarrays).
+        The updated variable, the new first moment,
+          and the new second moment, respectively (all as numpy.ndarrays).
     """
     v = beta1 * v + (1 - beta1) * grad
     s = beta2 * s + (1 - beta2) * (grad ** 2)
-    
     v_corrected = v / (1 - beta1 ** t)
     s_corrected = s / (1 - beta2 ** t)
-    
     var -= alpha * v_corrected / (np.sqrt(s_corrected) + epsilon)
-    
     return var, v, s
