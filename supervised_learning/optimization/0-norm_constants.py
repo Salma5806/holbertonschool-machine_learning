40c9c38a62b461dd42f7ad8c65879d1921e9bb77
Salma5806 <5806@holbertonstudents.com>
2023-09-03 18:52:00 +0100

    normal std

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""  calculates the normalization (standardization) constants of a matrix """
+
+import numpy as np
+
+
+def normalization_constants(X):
+    """
+    Calculate the mean and standard deviation of each feature in matrix X.
+
+    Args:
+    - X: numpy.ndarray of shape (m, nx) to calculate normalization constants
+      - m: number of data points
+      - nx: number of features
+
+    Returns:
+    - A tuple containing the mean and standard deviation of each feature, respectively.
+    """
+    mean = np.mean(X, axis=0)
+    std_dev = np.std(X, axis=0)
+
+    return mean, std_dev
