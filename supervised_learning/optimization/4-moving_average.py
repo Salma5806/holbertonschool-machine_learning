cf90c552a543b96c41192c2b8a1cbc5c7834c99f
Salma5806 <5806@holbertonstudents.com>
2023-09-03 19:31:47 +0100

    average data

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" calculates the weighted moving average of a data set """
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def moving_average(data, beta):
+    """
+    Calculates the weighted moving average of a data set.
+
+    Args:
+        data: List of data to calculate the moving average of.
+        beta: The weight used for the moving average.
+
+    Returns:
+        A list containing the moving averages of the data.
+    """
+    moving_avg = []
+    prev_avg = 0.0
+
+    for i, x in enumerate(data):
+        bias_correction = 1 - (beta ** (i + 1))
+        curr_avg = (beta * prev_avg) + ((1 - beta) * x / bias_correction)
+        moving_avg.append(curr_avg)
+        prev_avg = curr_avg
+
+    return moving_avg
