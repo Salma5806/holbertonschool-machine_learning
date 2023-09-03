75c40ce8daac07c056d68ca16dbb94e1c5c7f602
Salma5806 <5806@holbertonstudents.com>
2023-09-03 22:46:10 +0100

    batch layer

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -16,20 +16,11 @@ def create_batch_norm_layer(prev, n, activation):
     Returns:
         A tensor of the activated output for the layer.
     """
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    x = tf.layers.Dense(units=n, activation=None, kernel_initializer=init)
-    x_prev = x(prev)
-    scale = tf.Variable(tf.constant(1.0, shape=[n]), name='gamma')
-    mean, variance = tf.nn.moments(x_prev, axes=[0])
-    offset = tf.Variable(tf.constant(0.0, shape=[n]), name='beta')
-    variance_epsilon = 1e-8
-
-    normalization = tf.nn.batch_normalization(
-        x_prev,
-        mean,
-        variance,
-        offset,
-        scale,
-        variance_epsilon,
-    )
-    return activation(normalization)
+    kernel_initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.keras.layers.Dense(units=n, kernel_initializer=kernel_initializer, activation=None)(prev)
+    gamma = tf.Variable(initial_value=tf.ones((n,)), trainable=True)
+    beta = tf.Variable(initial_value=tf.zeros((n,)), trainable=True)
+    epsilon = 1e-8
+    mean, variance = tf.nn.moments(layer, axes=[0])
+    normalized = tf.nn.batch_normalization(layer, mean, variance, beta, gamma, epsilon)
+    return activation(normalized)
