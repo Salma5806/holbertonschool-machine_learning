ef06c9dda0e66f436c55ea5c3f16d21dbbf97465
Salma5806 <5806@holbertonstudents.com>
2023-09-03 20:22:08 +0100

    update RMSProp

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" creates the training operation for a neural network in tensorflow using 
+the RMSProp optimization algorithm """
+
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """
+    Updates a variable using the RMSProp optimization algorithm.
+
+    Args:
+        alpha: The learning rate.
+        beta2: The RMSProp weight.
+        epsilon: A small number to avoid division by zero.
+        var: A numpy.ndarray containing the variable to be updated.
+        grad: A numpy.ndarray containing the gradient of var.
+        s: The previous second moment of var.
+
+    Returns:
+        The updated variable and the new moment, respectively.
+    """
+    s = beta2 * s + (1 - beta2) * (grad ** 2)
+    var = var - alpha * grad / (np.sqrt(s) + epsilon)
+
+    return var, s
