6b303d7ef3f73adb6508c4f4e49bc8ac4b4fb9dd
Salma5806 <5806@holbertonstudents.com>
2023-09-03 19:52:16 +0100

    average data

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -16,13 +16,13 @@ def moving_average(data, beta):
         A list containing the moving averages of the data.
     """
     moving_averages = []
-    weighted_sum = 0.0
-    beta_power = 1.0
+    weighted_sum = 0
+    beta_power = 1
 
-    for i, value in enumerate(data):
-        weighted_sum = (beta * weighted_sum) + ((1 - beta) * value)
-        bias_correction = weighted_sum / (1 - beta_power)
-        moving_averages.append(bias_correction)
-        beta_power *= beta
+    for i, value in range(len(data)):
+        weighted_sum = (beta * weighted_sum) + ((1 - beta) * data[i])
+        bias_correction = 1 - beta ** (i + 1)
+        new_weight = weighted_sum / bias_correction
+        moving_averages.append(new_weight)
 
     return moving_averages
