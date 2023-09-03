716c3f4263835ff11b38032cfdc579cf37745ac9
Salma5806 <5806@holbertonstudents.com>
2023-09-03 22:38:03 +0100

    batch layer

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -16,10 +16,20 @@ def create_batch_norm_layer(prev, n, activation):
     Returns:
         A tensor of the activated output for the layer.
     """
-    kernel_initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    
-    dense_layer = tf.keras.layers.Dense(units=n, kernel_initializer=kernel_initializer, use_bias=False)
-    batch_norm_layer = tf.keras.layers.BatchNormalization(epsilon=1e-8)
-    output = activation(batch_norm_layer(dense_layer(prev)))
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    x = tf.layers.Dense(units=n, activation=None, kernel_initializer=init)
+    x_prev = x(prev)
+    scale = tf.Variable(tf.constant(1.0, shape=[n]), name='gamma')
+    mean, variance = tf.nn.moments(x_prev, axes=[0])
+    offset = tf.Variable(tf.constant(0.0, shape=[n]), name='beta')
+    variance_epsilon = 1e-8
 
-    return output
+    normalization = tf.nn.batch_normalization(
+        x_prev,
+        mean,
+        variance,
+        offset,
+        scale,
+        variance_epsilon,
+    )
+    return activation(normalization)
