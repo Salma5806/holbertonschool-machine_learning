84335f9e49bf317e81418aa0fb721004ebee2f0a
Salma5806 <5806@holbertonstudents.com>
2023-09-03 20:14:04 +0100

    momentum

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-""" updates a variable using the gradient descent with momentum optimization algorithm """
+""" updates a variable using the gradient
+descent with momentum optimization algorithm """
 
 import numpy as np
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """
-    Updates a variable using the gradient descent with momentum optimization algorithm.
+    Updates a variable using the gradient descent
+    with momentum optimization algorithm.
 
     Args:
         alpha: The learning rate.
@@ -14,7 +16,6 @@ def update_variables_momentum(alpha, beta1, var, grad, v):
         var: A numpy.ndarray containing the variable to be updated.
         grad: A numpy.ndarray containing the gradient of var.
         v: The previous first moment of var.
-
     Returns:
         A tuple containing the updated variable and the new moment, respectively.
     """
