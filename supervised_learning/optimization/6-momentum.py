39ac617ff0aaebccfd800d78c1be160a3b3d7ce1
Salma5806 <5806@holbertonstudents.com>
2023-09-03 20:10:12 +0100

    momentum upgrade

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""  creates the training operation for a neural network
+ in tensorflow using the gradient descent with momentum 
+ optimization algorithm
+ """
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """
+    Creates the training operation for a neural network in TensorFlow
+    using the gradient descent with momentum optimization algorithm.
+
+    Args:
+        loss: The loss of the network.
+        alpha: The learning rate.
+        beta1: The momentum weight.
+
+    Returns:
+        The momentum optimization operation.
+    """
+    optimizer = tf.train.MomentumOptimizer(learning_rate=alpha, momentum=beta1)
+    train_op = optimizer.minimize(loss)
+
+    return train_op
