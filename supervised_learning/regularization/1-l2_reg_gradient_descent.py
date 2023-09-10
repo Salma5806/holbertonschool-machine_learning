a5396ec504266fc86ba53de538d93b6b163352b7
Salma5806 <5806@holbertonstudents.com>
2023-09-10 21:40:41 +0100

    GD with regularization

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -11,19 +11,21 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """Updates weights and biases using gradient descent with L2 regularization"""
     m = Y.shape[1]
 
-    for i in range(L, 0, -1):
-        Ai = cache['A' + str(i)]
-        Ai_minus_1 = cache['A' + str(i - 1)]
-        Wi = weights['W' + str(i)]
-        bi = weights['b' + str(i)]
-        Zi = np.dot(Wi, Ai_minus_1) + bi
-        if i == L:
-            dZi = Ai - Y
+    for l in range(L, 0, -1):
+        Al = cache["A" + str(l)]
+        Al_1 = cache["A" + str(l - 1)]
+        Wl = weights["W" + str(l)]
+        bl = weights["b" + str(l)]
+        Al_1 = cache["A" + str(l - 1)]
+
+        if l == L:
+            dZl = Al - Y
         else:
-            dZi = np.dot(weights['W' + str(i + 1)].T, dZi) * (1 - np.power(Ai, 2))
+            dZl = dAl * (1 - (Al ** 2))  # Derivative of tanh
 
-        dWi = np.dot(dZi, Ai_minus_1.T) / m + (lambtha / m) * Wi
-        dbi = np.sum(dZi, axis=1, keepdims=True) / m
+        dWl = (1 / m) * np.matmul(dZl, Al_1.T) + (lambtha / m) * Wl
+        dbl = (1 / m) * np.sum(dZl, axis=1, keepdims=True)
 
-        weights['W' + str(i)] = weights['W' + str(i)] - alpha * dWi
-        weights['b' + str(i)] = weights['b' + str(i)] - alpha * dbi
+        dAl = np.matmul(Wl.T, dZl)
+        weights["W" + str(l)] -= alpha * dWl
+        weights["b" + str(l)] -= alpha * dbl
