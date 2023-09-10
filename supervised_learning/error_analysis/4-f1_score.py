052f1460ebd8d6bab50abf35d7f6b203b8f06030
Salma5806 <5806@holbertonstudents.com>
2023-09-10 22:24:26 +0100

    f1 score

---
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Script to calculate the f1-score in a
+    confusion matrix
+"""
+
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """Function to calculate the f1-score"""
+    p = precision(confusion)
+    s = sensitivity(confusion)
+    f1 = 2 * (p * s) / (p + s)
+    return f1
