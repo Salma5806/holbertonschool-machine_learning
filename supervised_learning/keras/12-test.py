13d32f067cbba317413df28ef267b936bccc1c5f
Salma5806 <5806@holbertonstudents.com>
2023-09-17 21:14:14 +0100

    test a neural network

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -7,7 +7,10 @@ import tensorflow.keras as K
 def test_model(network, data, labels, verbose=True):
     """
     Tests a neural network with input data and labels."""
-    loss, accuracy = network.evaluate(x=data,
-                                      y=labels,
-                                      verbose=verbose)
+    network.compile(loss='categorical_crossentropy',
+                      metrics=['accuracy'], optimizer='adam')
+    evaluation = network.evaluate(data, labels, verbose=verbose)
+    loss = evaluation[0]
+    accuracy = evaluation[1]
+    
     return loss, accuracy
