dded194974beed6daa51897cc124ca4eab22d3c3
Salma5806 <5806@holbertonstudents.com>
2023-09-17 20:56:53 +0100

    trains a model using mini batch

--- supervised_learning/keras/8-train.py
+++ supervised_learning/keras/8-train.py
@@ -8,7 +8,8 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, learning_rate_decay=False,
-                alpha=0.1, decay_rate=1, save_best=False, filepath=None, verbose=True, shuffle=False):
+                alpha=0.1, decay_rate=1, save_best=False,
+                filepath=None, verbose=True, shuffle=False):
     """
     Trains a model using mini-batch gradient descent and optionally
     validates it with learning rate decay."""
