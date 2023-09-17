f04091cf4faf3f21f848dd9a8630db8e577e4c71
Salma5806 <5806@holbertonstudents.com>
2023-09-17 20:15:41 +0100

    build model

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -9,23 +9,16 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     Builds a neural network with Keras 
     without using the Sequential class."""
     
-    x = K.layers.Input(shape=(nx,))
-    
-    intermediate_layers = []
-    
-    for i, (nodes, activation) in enumerate(zip(layers, activations)):
-        dense_layer = K.layers.Dense(
-            nodes,
-            activation=activation,
-            kernel_regularizer=K.regularizers.l2(lambtha),
-            name=f'dense_{i}'
-        )(x if i == 0 else intermediate_layers[-1])
-        
+    X_input = K.layers.Input(shape=(nx,))
+
+    X = X_input
+
+    for i in range(len(layers)):
+        X = K.layers.Dense(units=layers[i], activation=activations[i],
+                           kernel_regularizer=K.regularizers.l2(lambtha))(X)
+
         if i < len(layers) - 1:
-            dropout_layer = K.layers.Dropout(rate=1 - keep_prob, name=f'dropout_{i}')(dense_layer)
-            intermediate_layers.append(dropout_layer)
-        else:
-            intermediate_layers.append(dense_layer)
-    
-    model = K.models.Model(inputs=x, outputs=intermediate_layers[-1])
+            X = K.layers.Dropout(1 - keep_prob)(X)
+    model = K.models.Model(inputs=X_input, outputs=X)
+
     return model
