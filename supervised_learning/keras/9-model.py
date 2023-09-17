eafcddcfbfaf00ddfdb64dd9c95d0481bdaf9556
Salma5806 <5806@holbertonstudents.com>
2023-09-17 20:55:55 +0100

    save and load

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" saves an entire model and loads an entire model """
+
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """
+    Saves an entire Keras model to a file."""
+    network.save(filename)
+
+def load_model(filename):
+    """
+    Loads an entire Keras model from a file."""
+    loaded_model = K.models.load_model(filename)
+    return loaded_model
