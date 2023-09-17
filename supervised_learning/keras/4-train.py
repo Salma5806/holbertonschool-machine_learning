7b0350b9ff65e61c43fa4596f999251d2d81f3f2
Salma5806 <5806@holbertonstudents.com>
2023-09-17 20:31:59 +0100

    tains a model

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -4,7 +4,8 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, 
+                epochs, verbose=True, shuffle=False):
     """
     Trains a model using mini-batch gradient descent."""
     history = network.fit(
@@ -13,7 +14,5 @@ def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle
         batch_size=batch_size,
         epochs=epochs,
         verbose=verbose,
-        shuffle=shuffle
-    )
-    
+        shuffle=shuffle)
     return history
