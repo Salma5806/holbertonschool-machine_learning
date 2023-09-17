dc90559645aa76a9c10bc645f63e943963986893
Salma5806 <5806@holbertonstudents.com>
2023-09-17 20:35:10 +0100

    trains a model using mini batch

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -1,21 +1,13 @@
+#!/usr/bin/env python3
+""" trains a model """
+
 import tensorflow.keras as K
+
 
 def train_model(network, data, labels, batch_size, epochs, validation_data=None, verbose=True, shuffle=False):
     """
-    Trains a model using mini-batch gradient descent and optionally validates it.
-
-    Args:
-        network (Keras model): The model to train.
-        data (numpy.ndarray): Input data of shape (m, nx).
-        labels (numpy.ndarray): One-hot encoded labels of shape (m, classes).
-        batch_size (int): Size of the mini-batches.
-        epochs (int): Number of passes through the entire dataset.
-        validation_data (tuple): Validation data as a tuple (val_data, val_labels).
-        verbose (bool): Whether to print training progress (default is True).
-        shuffle (bool): Whether to shuffle the data between epochs (default is False).
-
-    Returns:
-        Keras History object: Contains training history information.
+    Trains a model using mini-batch gradient
+      descent and optionally validates it.
     """
     if validation_data:
         val_data, val_labels = validation_data
@@ -28,7 +20,7 @@ def train_model(network, data, labels, batch_size, epochs, validation_data=None,
         epochs=epochs,
         verbose=verbose,
         shuffle=shuffle,
-        validation_data=validation_data  # Add validation data here
+        validation_data=validation_data
     )
     
     return history
