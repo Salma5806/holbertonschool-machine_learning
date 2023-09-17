ca74b2d248494d9946f0c896a43c2ef82019476a
Salma5806 <5806@holbertonstudents.com>
2023-09-17 19:59:43 +0100

    build model with keras

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -3,18 +3,21 @@
 
 import tensorflow.keras as K
 
+
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Builds a neural network with Keras."""
     model = K.Sequential()
 
     for i in range(len(layers)):
         if i == 0:
-            model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                     kernel_regularizer=K.regularizers.l2(lambtha),
-                                     input_shape=(nx,)))
+            model.add(K.layers.Dense(
+                layers[i], activation=activations[i],
+                kernel_regularizer=K.regularizers.l2(lambtha),
+                input_shape=(nx,)))
         else:
-            model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                     kernel_regularizer=K.regularizers.l2(lambtha)))
+            model.add(K.layers.Dense(
+                layers[i], activation=activations[i],
+                kernel_regularizer=K.regularizers.l2(lambtha)))
 
         if i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
