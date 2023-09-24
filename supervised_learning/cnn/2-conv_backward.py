18f01601a9d9779b935993efc69b04aea3d75196
Salma5806 <5806@holbertonstudents.com>
2023-09-24 19:00:05 +0100

    cnn backward

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -9,34 +9,42 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     convolutional layer of a neural network
     """
     m, h_new, w_new, c_new = dZ.shape
+    m, h_new, w_new, c_new = dZ.shape
     h_prev, w_prev, c_prev = A_prev.shape[1:]
     kh, kw = W.shape[:2]
     sh, sw = stride
-    dA_prev = np.zeros_like(A_prev)
-    dW = np.zeros_like(W)
+
+    if padding == "valid":
+        ph = 0
+        pw = 0
+    else:
+        ph = int((h_new * sh - h_prev + kh - 1) / 2)
+        pw = int((w_new * sw - w_prev + kw - 1) / 2)
+
+    padded = np.pad(A_prev,
+                    ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                    'constant',
+                    constant_values=0)
+    dA_prev = np.zeros(padded.shape)
+    dW = np.zeros(W.shape)
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
 
-    if padding == "same":
-        pad_h = int(((h_prev - 1) * sh + kh - h_new) / 2)
-        pad_w = int(((w_prev - 1) * sw + kw - w_new) / 2)
-        A_prev_pad = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), 'constant',
-                    constant_values=0)
+    for img in range(m):
+        for channel in range(c_new):
+            for row in range(h_new):
+                for col in range(w_new):
+                    grad = W[:, :, :, channel] * dZ[img, row, col, channel]
+                    dA_prev[img,
+                            row * sh:row * sh + kh,
+                            col * sw:col * sw + kw,
+                            :] += grad
 
-    elif padding == "valid":
-        A_prev_pad = A_prev
-    for i in range(m):
-        a_prev_pad = A_prev_pad[i]
-        for h in range(h_new):
-            for w in range(w_new):
-                for c in range(c_new):
-                    vert_start = h * sh
-                    vert_end = vert_start + kh
-                    horiz_start = w * sw
-                    horiz_end = horiz_start + kw
+                    grad = padded[img,
+                                  row * sh:row * sh + kh,
+                                  col * sw:col * sw + kw,
+                                  :] * dZ[img, row, col, channel]
+                    dW[:, :, :, channel] += grad
 
-                    a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
-
-                    dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, c] * dZ[i, h, w, c]
-                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
+    dA_prev = dA_prev[:, ph:dA_prev.shape[1] - ph, pw:dA_prev.shape[2] - pw, :]
 
     return dA_prev, dW, db
