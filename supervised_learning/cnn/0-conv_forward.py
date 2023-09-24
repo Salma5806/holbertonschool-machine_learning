7f7034d6009b34b81901f21c320152ef480418c4
Salma5806 <5806@holbertonstudents.com>
2023-09-24 18:10:34 +0100

    frwd prpg over cnn

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -9,26 +9,28 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, c_prev, c_new = W.shape
     sh, sw = stride
-    if padding == "same":
-        h_out = int(np.ceil(float(h_prev) / float(sh)))
-        w_out = int(np.ceil(float(w_prev) / float(sw)))
-        pad_h = max(0, (h_out - 1) * sh + kh - h_prev)
-        pad_w = max(0, (w_out - 1) * sw + kw - w_prev)
-        A_prev = np.pad(A_prev, ((0, 0), (pad_h // 2, pad_h - pad_h // 2), (pad_w // 2, pad_w - pad_w // 2), (0, 0)), mode='constant')
+    if padding == 'same':
+        pad_h = int(((h_prev * (sh - 1)) - sh + kh) / 2)
+        pad_w = int(((w_prev * (sw - 1)) - sw + kw) / 2)
+    elif type(padding) == tuple:
+        pad_h, pad_w = padding
     else:
-        h_out = int(np.floor(float(h_prev - kh) / float(sh))) + 1
-        w_out = int(np.floor(float(w_prev - kw) / float(sw))) + 1
-
-    Z = np.zeros((m, h_out, w_out, c_new))
+        pad_h = 0
+        pad_w = 0
+    img_pad = np.pad(A_prev, ((0, 0), (pad_h, pad_h),
+                              (pad_w, pad_w), (0, 0)),
+                     'constant', constant_values=(0))
+    img_pad_h = img_pad.shape[1]
+    img_pad_w = img_pad.shape[2]
+    h_out = int((img_pad_h - kh) / sh) + 1
+    w_out = int((img_pad_w - kw) / sw) + 1
+    result = np.zeros((m, h_out, w_out, c_new))
     for i in range(h_out):
         for j in range(w_out):
-            h_start = i * sh
-            h_end = h_start + kh
-            w_start = j * sw
-            w_end = w_start + kw
-            A_slice = A_prev[:, h_start:h_end, w_start:w_end, :]
-            Z[:, i, j, :] = np.sum(np.multiply(A_slice, W), axis=(1, 2, 3)) + b
-
-    A = activation(Z)
-
-    return A
+            for k in range(c_new):
+                result[:, i, j, k] = np.sum(img_pad[:,
+                                                    i * sh: i * sh + kh,
+                                                    j * sw: j * sw + kw] *
+                                            W[:, :, :, k],
+                                            axis=(1, 2, 3))
+    return activation(result + b)
