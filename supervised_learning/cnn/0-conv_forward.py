b81acdf1372a56fbbb1c738b2c473457d84e971e
Salma5806 <5806@holbertonstudents.com>
2023-09-24 18:02:32 +0100

    frwd prpg over cnn

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -3,8 +3,6 @@
 import numpy as np
 
 
-import numpy as np
-
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """ performs forward propagation over a convolutional
         layer of a neural network """
