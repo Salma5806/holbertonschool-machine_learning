2e4e70639a2519d23120f5f4b421fb9999da1ee9
Salma5806 <5806@holbertonstudents.com>
2023-10-16 09:17:39 +0100

    initialize Yolo

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""this module contains the class Yolo"""
+import tensorflow.keras as K
+import numpy as np
+import os
+
+
+class Yolo:
+    """Yolo class"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """Constructor method"""
+        if not os.path.exists(model_path):
+            raise FileNotFoundError("Wrong model file path")
+
+        if not os.path.exists(classes_path):
+            raise FileNotFoundError("Wrong classes file path")
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            self.class_names = [line[:-1] for line in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
