0817162d6be919ba6bd5091dbfa0c4cc0445b8ac
Salma5806 <5806@holbertonstudents.com>
2024-08-10 21:01:49 +0100

    request

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -8,14 +8,14 @@ def availableShips(passengerCount):
     Function to get request
     Args:
         passengerCount: number of passangers
-    Returns: List of planets
+    Returns: List of starships
     """
     starships = []
-    url = 'https://swapi-api.hbtn.io/api/planets/'
+    url = 'https://swapi-api.hbtn.io/api/starships/'
     while url is not None:
         response = requests.get(url,
                                 headers={'Accept': 'application/json'},
-                                params={"term": 'planets'})
+                                params={"term": 'starships'})
         for ship in response.json()['results']:
             passenger = ship['passengers'].replace(',', '')
             if passenger.isnumeric() and int(passenger) >= passengerCount:
