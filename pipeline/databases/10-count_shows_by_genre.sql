890d89615de1472afb7d541ff8bade768058bd2b
Salma5806 <5806@holbertonstudents.com>
2024-08-11 22:48:11 +0100

    count

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- Lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
 -- INNER JOIN WITH GROUP BY
-SELECT g.genre AS genre, COUNT(s.id) AS number_of_shows
-FROM genres g
-JOIN shows s ON g.id = s.genre_id
-GROUP BY g.genre
+SELECT genre AS genre, COUNT(*) AS number_of_shows
+FROM shows
+GROUP BY genre
 ORDER BY number_of_shows DESC;
