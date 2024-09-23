21ab7e00746017c0e96b772e9522ffa3da05fad4
Salma5806 <5806@holbertonstudents.com>
2024-09-23 16:11:21 +0100

    inception block

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -1,26 +1,32 @@
 #!/usr/bin/env python3
-""" Inception Block """
+"""
+building the architecture
+needed to match the idea of the
+inception blocks
+"""
+
 import tensorflow.keras as K
 
 
 def inception_block(A_prev, filters):
-    """ inception block
-    inception block as described in Going
-    Deeper with Convolutions"""
-    F1, F3R, F3, F5R, F5, FPP = filters
-    OxO_1_input = K.layers.Conv2D(filters=F1, kernel_size=(1, 1),
-                                  padding='same', activation='relu')(A_prev)
-    OxO_conv3x3 = K.layers.Conv2D(
-        filters=F3R, kernel_size=(1, 1), activation='relu')(A_prev)
-    conv3x3 = K.layers.Conv2D(filters=F3, kernel_size=(
-        3, 3), padding='same', activation='relu')(OxO_conv3x3)
-    OxO_conv5x5 = K.layers.Conv2D(
-        filters=F5R, kernel_size=(1, 1), activation='relu')(A_prev)
-    conv5x5 = K.layers.Conv2D(filters=F5, kernel_size=(
-        5, 5), padding='same', activation='relu')(OxO_conv5x5)
-    max_pool = K.layers.MaxPooling2D(pool_size=(
-        3, 3), strides=(1, 1), padding='same')(A_prev)
-    OxO_pool = K.layers.Conv2D(filters=FPP, kernel_size=(
-        1, 1), padding='same', activation='relu')(max_pool)
-    output = K.layers.concatenate([OxO_1_input, conv3x3, conv5x5, OxO_pool])
-    return output
+    """
+    A_prev is the output from the previous layer
+    filters is a tuple or list containing F1, F3R, F3,F5R, F5, FPP, respectively:
+
+    F1 is the number of filters in the 1x1 convolution
+    F3R is the number of filters in the 1x1 convolution before the 3x3 convolution
+    F3 is the number of filters in the 3x3 convolution
+    F5R is the number of filters in the 1x1 convolution before the 5x5 convolution
+    F5 is the number of filters in the 5x5 convolution
+    FPP is the number of filters in the 1x1 convolution after the max pooling
+
+    """
+    C1 = K.layers.Conv2D(filters=filters[0], kernel_size=(1, 1), padding='same',activation='relu')(A_prev)
+    C11 = K.layers.Conv2D(filters=filters[1], kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
+    C12 = K.layers.Conv2D(filters=filters[3], kernel_size=(1, 1), padding='same',activation='relu')(A_prev)
+    P13 = K.layers.MaxPooling2D(pool_size=(3, 3),strides=(1, 1), padding='same')(A_prev)
+    C2 = K.layers.Conv2D(filters=filters[2], kernel_size=(3, 3), padding='same', activation='relu')(C11)
+    C21 = K.layers.Conv2D(filters=filters[4], kernel_size=(5, 5), padding='same',activation='relu')(C12)
+    C22 = K.layers.Conv2D(filters=filters[5], kernel_size=(1, 1), padding='same',activation='relu')(P13)
+    inception = K.layers.Concatenate(axis=-1)([C1, C2, C21, C22])
+    return inception
