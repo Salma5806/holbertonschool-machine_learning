bb6bac4c0dc7563cebc9bc991747f1546972f4be
Salma5806 <5806@holbertonstudents.com>
2024-09-25 16:39:54 +0100

    rnn encoder

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 import tensorflow as tf
 """
-
+task 0
 """
 
 
 class RNNEncoder(tf.keras.layers.Layer):
     """
-
+    Class to encode for machine translation
     """
     def __init__(self, vocab, embedding, units, batch):
         """
-
+        Class constructor
         """
         super(RNNEncoder, self).__init__()
         self.batch = batch
@@ -25,13 +25,13 @@ class RNNEncoder(tf.keras.layers.Layer):
 
     def initialize_hidden_state(self):
         """
-
+        Initializes the hidden states for the RNN cell to a tensor of zeros
         """
         return tf.zeros((self.batch, self.units))
 
     def call(self, x, initial):
         """
-
+        Calls the encoder with given input to encoder layer and returns output
         """
         x = self.embedding(x)
         outputs, hidden = self.gru(x, initial_state=initial)
