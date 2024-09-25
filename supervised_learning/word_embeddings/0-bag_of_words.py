16c1e8aebcbeded270ef9dafb7acbbc6473462ef
Salma5806 <5806@holbertonstudents.com>
2024-09-25 15:36:46 +0100

    word

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python3
+"""Task 0"""
+
 import numpy as np
 import re
 
 
-"""
-
-"""
-
-
-def preprocess_sentence(sentence):
-    """
-
-    """
-    processed_sentence = re.sub(r"\b(\w+)'s\b", r"\1", sentence.lower())
-    return re.findall(r'\w+', processed_sentence)
-
-
 def bag_of_words(sentences, vocab=None):
     """
-
+    Creates a bag of words embedding matrix.
+      - sentences (list): A list of sentences to analyze.
+      - vocab (list, optional): A list of the vocabulary words to
+        use for the analysis.
+        If None, all words within sentences should be used.
+      - embeddings (numpy.ndarray): A 2D array of shape (s, f)
+        containing the embeddings, where s is the number of sentences
+        and f is the number of features.
+      - vocab (list): A list of the features used for embeddings.
+      - Tokenize the sentences into words, ignoring punctuation
+        (except in numbers) and converting to lowercase.
+      - If no vocabulary is provided, create one from the unique
+        words in the sentences.
+      - Create an empty matrix for the embeddings.
+      - Fill in the matrix with the word counts.
     """
-    preprocessed_sentences = [preprocess_sentence(sentence) 
-                        for sentence in sentences]
+
+    words_list = [re.findall(r'\b\w[\w]*\b', re.sub(
+      r"'s\b", '', sentence.lower())) for sentence in sentences]
+
     if vocab is None:
-        all_words = [word for sentence in preprocessed_sentences
-                    for word in sentence]
-        vocab = sorted(set(all_words))
-    word_to_index = {word: idx for idx, word in enumerate(vocab)}
+        vocab = sorted(set(word for words in words_list for word in words))
+
     embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
-    for i, sentence in enumerate(preprocessed_sentences):
-        for word in sentence:
-            if word in word_to_index:
-                embeddings[i, word_to_index[word]] += 1
+
+    for i, words in enumerate(words_list):
+        for word in words:
+            if word in vocab:
+                j = vocab.index(word)
+                embeddings[i, j] += 1
+
     return embeddings, vocab
