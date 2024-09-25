6e40578aba5e7ad07e93b465b06b4d0b94af98b4
Salma5806 <5806@holbertonstudents.com>
2024-09-25 15:35:09 +0100

    word

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,36 +1,24 @@
 #!/usr/bin/env python3
-"""Task 0"""
-
 import numpy as np
 import re
 
 
-def bag_of_words(sentence):
-    """
-    Creates a bag of words embedding matrix.
-      - sentences (list): A list of sentences to analyze.
-      - vocab (list, optional): A list of the vocabulary words to
-        use for the analysis.
-        If None, all words within sentences should be used.
-      - embeddings (numpy.ndarray): A 2D array of shape (s, f)
-        containing the embeddings, where s is the number of sentences
-        and f is the number of features.
-      - vocab (list): A list of the features used for embeddings.
-      - Tokenize the sentences into words, ignoring punctuation
-        (except in numbers) and converting to lowercase.
-      - If no vocabulary is provided, create one from the unique
-        words in the sentences.
-      - Create an empty matrix for the embeddings.
-      - Fill in the matrix with the word counts.
+"""
+
+"""
+
+
+def preprocess_sentence(sentence):
     """
 
+    """
     processed_sentence = re.sub(r"\b(\w+)'s\b", r"\1", sentence.lower())
     return re.findall(r'\w+', processed_sentence)
 
 
 def bag_of_words(sentences, vocab=None):
     """
-    badding word
+
     """
     preprocessed_sentences = [preprocess_sentence(sentence) 
                         for sentence in sentences]
