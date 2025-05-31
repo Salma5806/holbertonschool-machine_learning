4e47d4ea82243ceaf3cdb88057117e2129dcea6e
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-05-31 21:27:21 +0100

    Add files via upload

---
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+SelfAttention = __import__('1-self_attention').SelfAttention
+"""
+
+"""
+class RNNDecoder(tf.keras.layers.Layer):
+    """
+    Class to decode for machine translation
+    """
+    def __init__(self, vocab, embedding, units, batch):
+        """
+        Class constructor"""
+        super(RNNDecoder, self).__init__()
+        self.embedding = tf.keras.layers.Embedding(input_dim=vocab,
+                                                   output_dim=embedding)
+        self.gru = tf.keras.layers.GRU(units=units,
+                                       return_state=True,
+                                       return_sequences=True,
+                                       recurrent_initializer="glorot_uniform")
+        self.F = tf.keras.layers.Dense(units=vocab)
+
+    def call(self, x, s_prev, hidden_states):
+        """
+        Returns the output word as a one hot vector and
+            the new decoder hidden state
+
+        """
+        units = s_prev.get_shape().as_list()[1]
+        attention = SelfAttention(units)
+        context, weights = attention(s_prev, hidden_states)
+        x = self.embedding(x)
+        context = tf.expand_dims(context, 1)
+        x = tf.concat([context, x], axis=-1)
+        y, s = self.gru(x)
+        y = tf.reshape(y, (-1, y.shape[2]))
+        y = self.F(y)
+        return y, s
