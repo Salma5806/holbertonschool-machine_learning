7a1805fddd5463bee6cd8cb65153b9f9a6b351ed
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-05-31 22:09:35 +0100

    Update 5-sdp_attention.py

--- supervised_learning/attention/5-sdp_attention.py
+++ supervised_learning/attention/5-sdp_attention.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
-import tensorflow as tf
-"""
-Direct application of the
-equation
-"""
-def sdp_attention(Q, K, V, mask=None):
-    """
-    Applies scaled dot product attention to the inputs Q, K, and V."""
-    Q = tf.convert_to_tensor(Q, dtype='float32')
-    K = tf.convert_to_tensor(K, dtype='float32')
-    V = tf.convert_to_tensor(V, dtype='float32')
-    MatMul = tf.matmul(Q, K, transpose_b=True)
-    scale = 1 / tf.math.sqrt(tf.cast(tf.shape(Q)[-1], tf.float32))
-    scaled_score = MatMul * scale
-    if mask is not None:
-        scaled_score = scaled_score + (mask * -1e9)
+"""Scaled dot product attention"""
 
-    weights = tf.nn.softmax(scaled_score, axis=-1)
-    output = tf.matmul(weights, V)
-    return output, weights
+import numpy as np
+
+
+def sdp_attention(Q, K, V, mask=None):
+    """Calculate the scaled dot product attention"""
+    matmul_qk = np.matmul(Q, K.transpose(0, 1, 3, 2))
+    dk = K.shape[-1]
+    scaled_attention_logits = matmul_qk / np.sqrt(dk)
+
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    attention_weights = softmax(scaled_attention_logits)
+    output = np.matmul(attention_weights, V)
+
+    return output, attention_weights
+
+
+def softmax(x):
+    """Apply softmax to the last axis"""
+    e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+    return e_x / np.sum(e_x, axis=-1, keepdims=True)
