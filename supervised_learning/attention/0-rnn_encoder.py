b9c721908a1da2ccc15ce76d17b4732c6273e8b4
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-05-31 19:58:01 +0100

    Update 0-rnn_encoder.py

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
+"""
+RNNEncoder module for machine translation"""
+
 import tensorflow as tf
-"""
-"""
 
 
 class RNNEncoder(tf.keras.layers.Layer):
     """
+    RNNEncoder class that encodes input sequences using a GRU layer.
     """
+
     def __init__(self, vocab, embedding, units, batch):
         """
-        """
+        Class constructor"""
         super(RNNEncoder, self).__init__()
         self.batch = batch
         self.units = units
@@ -22,12 +25,12 @@ class RNNEncoder(tf.keras.layers.Layer):
 
     def initialize_hidden_state(self):
         """
-        """
+        Initializes the hidden state to a tensor of zeros"""
         return tf.zeros((self.batch, self.units))
 
     def call(self, x, initial):
         """
-        """
+        Performs the forward pass of the encoder"""
         x = self.embedding(x)
         outputs, hidden = self.gru(x, initial_state=initial)
         return outputs, hidden
