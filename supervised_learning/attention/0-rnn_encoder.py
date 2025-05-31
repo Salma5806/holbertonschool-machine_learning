2167dea7cf148c2fbd1623f3fc1920f994ce3ef4
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-05-31 19:51:43 +0100

    Update 0-rnn_encoder.py

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-"""RNN Encoder for Sequence Data"""
 import tensorflow as tf
-tf.compat.v1.enable_eager_execution()
+"""
+"""
 
-class RNNEncoder:
+
+class RNNEncoder(tf.keras.layers.Layer):
+    """
+    """
     def __init__(self, vocab, embedding, units, batch):
-        """ """
+        """
+        """
+        super(RNNEncoder, self).__init__()
         self.batch = batch
         self.units = units
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
@@ -14,16 +19,15 @@ class RNNEncoder:
                                        recurrent_initializer=init,
                                        return_sequences=True,
                                        return_state=True)
-        def initialize_hidden_state(self):
-            """ """
-            # initialise the hidden state to zeros
-             return tf.zeros((self.batch, self.units))
-        
-        def call(self, x, initial):
-            """
-            Forward pass through the RNN Encoder."""
-            x = self.embedding(x)  # (batch, input_seq_len, embedding_dim)
 
-            outputs, hidden = self.gru(x, initial_state=initial)
+    def initialize_hidden_state(self):
+        """
+        """
+        return tf.zeros((self.batch, self.units))
 
-            return outputs, hidden
+    def call(self, x, initial):
+        """
+        """
+        x = self.embedding(x)
+        outputs, hidden = self.gru(x, initial_state=initial)
+        return outputs, hidden
