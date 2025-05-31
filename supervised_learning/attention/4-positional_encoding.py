75e5c6ade3ff6d81efe7fbebb9be7579dbdbb042
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-05-31 21:59:40 +0100

    Update 4-positional_encoding.py

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
+"""
+positional_encoding.py
+Calculates the positional encoding for a transformer
+"""
+
 import numpy as np
-"""positional_encoding.py
-Calculates the positional encoding for a transformer"""
+
 
 def positional_encoding(max_seq_len, dm):
-    """"Calculates the positional encoding for a transformer"""
+    """
+    Calculates the positional encoding for a transformer
+
+    Args:
+        max_seq_len (int): maximum sequence length
+        dm (int): model depth (dimension)
+
+    Returns:
+        numpy.ndarray of shape (max_seq_len, dm) with positional encodings
+    """
     pe = np.zeros((max_seq_len, dm))
     for i in range(max_seq_len):
         for m in range(dm):
-            if int(m) % 2 == 0:
-                pe[i,  m] = np.sin(i / (10000 ** (m/ dm)))
+            angle = i / np.power(10000, (2 * (m // 2)) / dm)
+            if m % 2 == 0:
+                pe[i, m] = np.sin(angle)
             else:
-                pe[i, m] = np.cos(i / (10000 ** (m / dm)))
+                pe[i, m] = np.cos(angle)
     return pe
