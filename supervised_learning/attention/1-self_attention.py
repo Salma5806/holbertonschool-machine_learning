9d218b06761dbea9b289d96c2e021dcf229bc1ed
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-05-31 20:28:46 +0100

    Update 1-self_attention.py

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
-import tensorflow as tf
 """
-self attention layer
+Self-attention layer
 This module implements a self-attention layer using TensorFlow.
 """
 
-class selfAttention(tf.keras.layers.Layer):
+import tensorflow as tf
+
+class SelfAttention(tf.keras.layers.Layer):
     """
-    Self-Attention Layer
+    Self-Attention Layer that computes a context vector using attention mechanism.
     """
+
     def __init__(self, units):
         """
-        Initialize the self-attention layer.
+        Initializes the self-attention layer.
+
+        Args:
+            units (int): Number of hidden units in the dense layers.
         """
-        super(selfAttention, self).__init__()
+        super(SelfAttention, self).__init__()
         self.units = units
         self.W = tf.keras.layers.Dense(units)
         self.U = tf.keras.layers.Dense(units)
         self.V = tf.keras.layers.Dense(1)
+
+        # Print types and units
+        print(type(self.W), self.W.units)
+        print(type(self.U), self.U.units)
+        print(type(self.V), self.V.units)
 
     def call(self, s_prev, hidden_states):
         """
@@ -26,11 +36,7 @@ class selfAttention(tf.keras.layers.Layer):
         w_s_prev = self.W(s_prev)
         w_s_prev_expanded = tf.expand_dims(w_s_prev, 1)
         u_hidden_states = self.U(hidden_states)
-        somme = w_s_prev_expanded + u_hidden_states
-        tanh_somme = tf.nn.tanh(somme)
-        score = self.V(tanh_somme)
+        score = self.V(tf.nn.tanh(w_s_prev_expanded + u_hidden_states))
         attention_weights = tf.nn.softmax(score, axis=1)
-        context_vector = attention_weights * hidden_states
-        context_vector = tf.reduce_sum(context_vector, axis=1)
-
+        context_vector = tf.reduce_sum(attention_weights * hidden_states, axis=1)
         return context_vector, attention_weights
