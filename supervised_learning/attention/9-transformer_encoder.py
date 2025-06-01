d5db2b25ad7ec16ed19b1bc4faf05649ef3e1526
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-01 23:27:08 +0100

    Update 9-transformer_encoder.py

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -9,40 +9,24 @@ EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
 class Encoder(tf.keras.layers.Layer):
-    """
-    encoder for a transformer
-    """
-
-    def __init__(
-            self,
-            N,
-            dm,
-            h,
-            hidden,
-            input_vocab,
-            max_seq_len,
-            drop_rate=0.1):
-        """
-        Class constructor
-        """
-        super(Encoder, self).__init__()
-        self.N = N
-        self.dm = dm
-        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
-        self.positional_encoding = positional_encoding(max_seq_len, dm)
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
-                    for _ in range(N)]
-        self.dropout = tf.keras.layers.Dropout(drop_rate)
-
-    def call(self, x, training, mask):
-        """
-        Call method
-        """
-        seq_len = tf.shape(x)[1]
+   def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
+                 drop_rate=0.1):
+       self.N =N
+       self.dm = dm
+       self.h = h
+       self.hidden = hidden
+       self.input_vocab = input_vocab
+       self.max_seq_len = max_seq_len
+       self.drop_rate = drop_rate
+       self.embedding = tf.keras.layers.Embedding(input_dim=input_vocab,
+                                                   output_dim=dm)
+       self.positional_encoding = positional_encoding(max_seq_len, dm)
+       for block in range(N):
+        self.blocks = EncoderBlock(dm, h, hidden, drop_rate)
+       self.dropout = Dropout(drop_rate)
+       def call(self, x, training, mask=None):
         x = self.embedding(x)
-        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
-        x += self.positional_encoding[:seq_len]
+        x = x + self.positional_encoding
+        x = self.blocks(x, training, mask)
         x = self.dropout(x, training=training)
-        for block in self.blocks:
-            x = block(x, training, mask)
         return x
