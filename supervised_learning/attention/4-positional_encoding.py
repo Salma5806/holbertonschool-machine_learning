67728f05388a0d5b7c1e2619253135b90a060d3e
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-01 23:24:59 +0100

    Update 4-positional_encoding.py

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -5,10 +5,10 @@ Calculates the positional encoding for a transformer"""
 
 def positional_encoding(max_seq_len, dm):
     """"Calculates the positional encoding for a transformer"""
-    pos_encoding = np.zeros((max_seq_len, dm))
+    pe = np.zeros((max_seq_len, dm))
     for i in range(dm):
-        for pos in range(max_seq_len):
-            pos_encoding[pos, i] = pos / np.power(10000, (2 * (i // 2) / dm))
-    pos_encoding[:, 0::2] = np.sin(pos_encoding[:, 0::2])
-    pos_encoding[:, 1::2] = np.cos(pos_encoding[:, 1::2])
-    return pos_encoding
+        for m in range(max_seq_len):
+            pe[m, i] = pos / np.power(10000, (2 * (i // 2) / dm))
+    pe[:, 0::2] = np.sin(pe[:, 0::2])
+    pe[:, 1::2] = np.cos(pe[:, 1::2])
+    return pe
