415590c32ad8b20b0638c15622b228a24d172d6e
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-01 23:32:08 +0100

    Update 9-transformer_encoder.py

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
+"""
+EncoderBlock: A building block of the Transformer encoder.
+It applies multi-head self-attention followed by a feed-forward network,
+with residual connections, layer normalization, and dropout.
+"""
 
-"""
-transformer encoder
-"""
 import tensorflow as tf
-positional_encoding = __import__('4-positional_encoding').positional_encoding
-EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
 
+class EncoderBlock(tf.keras.layers.Layer):
+    """
+    Transformer Encoder Block(multihead)"""
 
-class Encoder(tf.keras.layers.Layer):
-   def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
-                 drop_rate=0.1):
-       self.N =N
-       self.dm = dm
-       self.h = h
-       self.hidden = hidden
-       self.input_vocab = input_vocab
-       self.max_seq_len = max_seq_len
-       self.drop_rate = drop_rate
-       self.embedding = tf.keras.layers.Embedding(input_dim=input_vocab,
-                                                   output_dim=dm)
-       self.positional_encoding = positional_encoding(max_seq_len, dm)
-       for block in range(N):
-        self.blocks = EncoderBlock(dm, h, hidden, drop_rate)
-       self.dropout = Dropout(drop_rate)
-       def call(self, x, training, mask=None):
-        x = self.embedding(x)
-        x = x + self.positional_encoding
-        x = self.blocks(x, training, mask)
-        x = self.dropout(x, training=training)
-        return x
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        """Initializes the encoder block(dimen, head, hidden"""
+        super().__init__()
+        self.mha = MultiHeadAttention(dm, h)
+        self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
+        self.dense_output = tf.keras.layers.Dense(dm)
+        self.norm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.norm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask=None):
+        """Runs the encoder block on the input"""
+        attn_output, _ = self.mha(x, x, x, mask)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.norm1(x + attn_output)
+        ffn_output = self.dense_hidden(out1)
+        ffn_output = self.dense_output(ffn_output)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.norm2(out1 + ffn_output)
+
+        return out2
