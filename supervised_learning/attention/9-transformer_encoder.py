58cca920132a07f59b084b4078b3690c0757cb1f
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-01 22:51:20 +0100

    Update 9-transformer_encoder.py

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -7,29 +7,23 @@ EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
 class Encoder(tf.keras.layers.Layer):
-    """Classe représentant un bloc d'encodeur du Transformer"""
-    def __init__(self, dm, h, hidden, drop_rate=0.1):
-        """Initialise un bloc encodeur"""
-        super().__init__()
-
-        self.mha = tf.keras.layers.MultiHeadAttention(num_heads=h, key_dim=dm // h)
-        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
-        self.norm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-
-        self.ffn = tf.keras.Sequential([
-            tf.keras.layers.Dense(hidden, activation='relu'),
-            tf.keras.layers.Dense(dm)
-        ])
-        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
-        self.norm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+    def __init__(self, N, dm, h, hidden,
+                 input_vocab, max_seq_len, drop_rate=0.1):
+        super(Encoder, self).__init__()
+        self.N = N
+        self.dm = dm
+        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        self.blocks = [EncoderBlock(dm, h, hidden,
+                                    drop_rate) for i in range(N)]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask):
-        """Applique le bloc encodeur sur les entrées"""
-        attn_output = self.mha(x, x, x, attention_mask=mask)
-        attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.norm1(x + attn_output)
-        ffn_output = self.ffn(out1)
-        ffn_output = self.dropout2(ffn_output, training=training)
-        out2 = self.norm2(out1 + ffn_output)
-
-        return out2
+        seq_len = tf.shape(x)[1]
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        x += self.positional_encoding[:seq_len, :]
+        x = self.dropout(x, training=training)
+        for block in self.blocks:
+            x = block(x, training, mask)
+        return x
