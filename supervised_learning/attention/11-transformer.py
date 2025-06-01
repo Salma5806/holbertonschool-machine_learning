b46125d6c36fcc9441d1ef4747520534c5094061
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-01 23:45:07 +0100

    Update 11-transformer.py

--- supervised_learning/attention/11-transformer.py
+++ supervised_learning/attention/11-transformer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Transformer Network: Combines encoder and decoder into a complete Transformer model.
+Transformer Network: Combines encoder and decoder into a complete Transformer.
 """
 
 import tensorflow as tf
@@ -12,25 +12,29 @@ class Transformer(tf.keras.Model):
     """
     Transformer Model
 
-    This class implements a complete Transformer architecture, 
-    combining both the encoder and the decoder for tasks such as 
-    machine translation or sequence-to-sequence prediction.
+    Combines encoder and decoder into a full transformer architecture for
+    sequence-to-sequence tasks like translation or summarization.
     """
 
     def __init__(self, N, dm, h, hidden, input_vocab,
-                 target_vocab, max_seq_input, max_seq_target, drop_rate=0.1):
+                 target_vocab, max_seq_input, max_seq_target,
+                 drop_rate=0.1):
         """Initializes the Transformer model"""
         super().__init__()
-        self.encoder = Encoder(N, dm, h, hidden, input_vocab, max_seq_input, drop_rate)
-        self.decoder = Decoder(N, dm, h, hidden, target_vocab, max_seq_target, drop_rate)
+        self.encoder = Encoder(
+            N, dm, h, hidden, input_vocab, max_seq_input, drop_rate
+        )
+        self.decoder = Decoder(
+            N, dm, h, hidden, target_vocab, max_seq_target, drop_rate
+        )
         self.linear = tf.keras.layers.Dense(target_vocab)
 
     def call(self, inputs, target, training,
              encoder_mask, look_ahead_mask, decoder_mask):
         """Performs the forward pass of the transformer model"""
         encoder_output = self.encoder(inputs, training, encoder_mask)
-        decoder_output = self.decoder(target, encoder_output, training,
-                                      look_ahead_mask, decoder_mask)
+        decoder_output = self.decoder(
+            target, encoder_output, training, look_ahead_mask, decoder_mask
+        )
         final_output = self.linear(decoder_output)
-
         return final_output
