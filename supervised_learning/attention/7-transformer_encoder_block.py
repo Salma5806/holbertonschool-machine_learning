49ec92b1996f3b6d0f9e40062c2b221b5a2cc65f
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-01 21:51:53 +0100

    Update 7-transformer_encoder_block.py

--- supervised_learning/attention/7-transformer_encoder_block.py
+++ supervised_learning/attention/7-transformer_encoder_block.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
-"""Module implementing the Multi Head Attention layer."""
+"""Module implementing the Transformer Encoder Block."""
+
 import tensorflow as tf
 MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
 
+
 class EncoderBlock(tf.keras.layers.Layer):
-    """Bloc d'encodage pour un transformeur, avec attention multi-têtes et réseau feed-forward."""
+    """Transformer encoder block with multi-head attention and feed-forward network"""
 
     def __init__(self, dm, h, hidden, drop_rate=0.1):
-        """Initialise le bloc encodeur"""
+        """Initializes the encoder block"""
         super(EncoderBlock, self).__init__()
         self.dm = dm
         self.h = h
         self.hidden = hidden
         self.drop_rate = drop_rate
+
         self.mha = MultiHeadAttention(dm, h)
         self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
         self.dense_output = tf.keras.layers.Dense(dm)
+
         self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
         self.dropout1 = tf.keras.layers.Dropout(drop_rate)
         self.dropout2 = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask=None):
-        """Passe avant du bloc encodeur"""
+        """Forward pass for the encoder block"""
         output_1, _ = self.mha(x, x, x, mask)
         output_1 = self.dropout1(output_1, training=training)
         out_1 = self.layernorm1(x + output_1)
+
         output_2 = self.dense_hidden(out_1)
         output_2 = self.dense_output(output_2)
         output_2 = self.dropout2(output_2, training=training)
