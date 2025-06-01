5ef73c0880ecc3dafd4e3bf4b822aa5724c785b6
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-01 23:25:47 +0100

    Update 4-positional_encoding.py

--- supervised_learning/attention/4-positional_encoding.py
+++ supervised_learning/attention/4-positional_encoding.py
@@ -8,7 +8,7 @@ def positional_encoding(max_seq_len, dm):
     pe = np.zeros((max_seq_len, dm))
     for i in range(dm):
         for m in range(max_seq_len):
-            pe[m, i] = pos / np.power(10000, (2 * (i // 2) / dm))
+            pe[m, i] = m / np.power(10000, (2 * (i // 2) / dm))
     pe[:, 0::2] = np.sin(pe[:, 0::2])
     pe[:, 1::2] = np.cos(pe[:, 1::2])
     return pe
