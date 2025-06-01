e0ed104190edbac1d7de1010f30c9c79814fb4cf
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-01 22:32:29 +0100

    Add files via upload

---
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Module implementing the Transformer Encoder."""
+
+import tensorflow as tf
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+
+
+class Encoder(tf.keras.layers.Layer):
+    """Transformer Encoder composed of stacked EncoderBlocks."""
+
+    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1):
+        """
+        Class constructor for the Transformer Encoder.
+        """
+        self.N =N
+        self.dm = dm
+        self.h = h
+        self.hidden = hidden
+        self.input_vocab = input_vocab
+        self.max_seq_len = max_seq_len
+        self.drop_rate = drop_rate
+        self.embedding = tf.keras.layers.Embedding(input_dim=input_vocab,
+                                                    output_dim=dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        for block in range(N):
+            self.blocks = EncoderBlock(dm, h, hidden, drop_rate)
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask):
+        """
+        Forward pass of the encoder.
+        """
+        x = self.embedding(x)
+        x = x + self.positional_encoding
+        x = self.blocks(x, training, mask)
+        x = self.dropout(x, training=training)
+        return x
