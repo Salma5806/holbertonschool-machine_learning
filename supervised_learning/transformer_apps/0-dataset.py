64e7f3269f66305107faf8914dcfa8dbd93f7be0
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-08 20:34:17 +0100

    Add files via upload

---
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Task 0"""
+
+import tensorflow_datasets as tfds
+
+
+class Dataset:
+    """
+    This class handles the loading and tokenization of the
+    ted_hrlr_translate/pt_to_en dataset from TensorFlow Datasets.
+    """
+
+    def __init__(self):
+        """
+        Constructor method. Loads the train and
+        validation splits of the dataset and tokenizes them.
+        """
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation', as_supervised=True)
+
+        self.tokenizer_pt = AutoTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
+        self.tokenizer_en = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+    def tokenize_dataset(self, data):
+        """Tokenizes the dataset"""
+       	tokenized_pt, tokenized_en = [], []
+        for pt, en in data:
+            pt_str = pt.numpy().decode('utf-8')
+            en_str = en.numpy().decode('utf-8')
+            tokenized_pt.append(self.tokenizer_pt.encode(pt_str))
+            tokenized_en.append(self.tokenizer_en.encode(en_str))
+        return tokenized_pt, tokenized_en
---
+++ supervised_learning/transformer_apps/2-dataset.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""Task 2"""
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import numpy as np
+
+
+class Dataset:
+    """
+    This class handles the loading and tokenization of the
+    ted_hrlr_translate/pt_to_en dataset from TensorFlow Datasets.
+    """
+
+    def __init__(self):
+        """
+        Constructor method. Loads the train and
+        validation splits of the dataset and tokenizes them.
+        """
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation', as_supervised=True)
+
+        self.tokenizer_pt = AutoTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
+        self.tokenizer_en = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+    def tokenize_dataset(self, data):
+        """Tokenizes the dataset"""
+       	tokenized_pt, tokenized_en = [], []
+        for pt, en in data:
+            pt_str = pt.numpy().decode('utf-8')
+            en_str = en.numpy().decode('utf-8')
+            tokenized_pt.append(self.tokenizer_pt.encode(pt_str))
+            tokenized_en.append(self.tokenizer_en.encode(en_str))
+        return tokenized_pt, tokenized_en
+
+    def encode(self, pt, en):
+        """Encodes a translation pair into tokens"""
+        pt_str = pt.numpy().decode('utf-8')
+        en_str = en.numpy().decode('utf-8')
+        pt_tokens = self.tokenizer_pt.encode(pt_str)
+        en_tokens = self.tokenizer_en.encode(en_str)
+        pt_tokens = [self.pt_vocab_size] + pt_tokens + [self.pt_vocab_size + 1]
+        en_tokens = [self.en_vocab_size] + en_tokens + [self.en_vocab_size + 1]
+        return np.array(pt_tokens), np.array(en_tokens)
+    def tf_encode(self, pt, en):
+        """Wraps the encode method in a TensorFlow py_function"""
+        pt, en = tf.py_function(self.encode, [pt, en], [tf.int32, tf.int32])
+        pt.set_shape([None])
+        en.set_shape([None])
+        return pt, en
