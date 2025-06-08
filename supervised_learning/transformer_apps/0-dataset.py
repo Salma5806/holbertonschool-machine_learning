e49e103089596cc3b0ab800e9282635fd8f2afd3
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-08 20:41:58 +0100

    Update 0-dataset.py

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -2,15 +2,14 @@
 """Task 0"""
 
 import tensorflow_datasets as tfds
+import transformers
 from transformers import AutoTokenizer
-
 
 class Dataset:
     """
     This class handles the loading and tokenization of the
     ted_hrlr_translate/pt_to_en dataset from TensorFlow Datasets.
     """
-
     def __init__(self):
         """
         Constructor method. Loads the train and
@@ -20,13 +19,12 @@ class Dataset:
                                     split='train', as_supervised=True)
         self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
                                     split='validation', as_supervised=True)
-
         self.tokenizer_pt = AutoTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
         self.tokenizer_en = AutoTokenizer.from_pretrained("bert-base-uncased")
 
     def tokenize_dataset(self, data):
         """Tokenizes the dataset"""
-       	tokenized_pt, tokenized_en = [], []
+        tokenized_pt, tokenized_en = [], []
         for pt, en in data:
             pt_str = pt.numpy().decode('utf-8')
             en_str = en.numpy().decode('utf-8')
