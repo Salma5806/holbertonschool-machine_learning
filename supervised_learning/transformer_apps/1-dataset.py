475be8505e92f15bcd68e4bf52fe976a287f4abb
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-08 21:19:08 +0100

    Update 1-dataset.py

--- supervised_learning/transformer_apps/1-dataset.py
+++ supervised_learning/transformer_apps/1-dataset.py
@@ -1,46 +1,60 @@
 #!/usr/bin/env python3
-"""Task 1"""
 
-import tensorflow as tf
+"""task 0"""
+
 import tensorflow_datasets as tfds
-import numpy as np
+import transformers
 
 
 class Dataset:
     """
-    This class handles the loading and tokenization of the
-    ted_hrlr_translate/pt_to_en dataset from TensorFlow Datasets.
+    This class loads and preps a dataset for machine translation
     """
 
     def __init__(self):
         """
-        Constructor method. Loads the train and
-        validation splits of the dataset and tokenizes them.
+        Class constructor
         """
         self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='train', as_supervised=True)
+                                    split='train',
+                                    as_supervised=True)
         self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='validation', as_supervised=True)
-
-        self.tokenizer_pt = AutoTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
-        self.tokenizer_en = AutoTokenizer.from_pretrained("bert-base-uncased")
+                                    split='validation',
+                                    as_supervised=True)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train
+        )
 
     def tokenize_dataset(self, data):
-        """Tokenizes the dataset"""
-       	tokenized_pt, tokenized_en = [], []
-        for pt, en in data:
-            pt_str = pt.numpy().decode('utf-8')
-            en_str = en.numpy().decode('utf-8')
-            tokenized_pt.append(self.tokenizer_pt.encode(pt_str))
-            tokenized_en.append(self.tokenizer_en.encode(en_str))
-        return tokenized_pt, tokenized_en
+        """Creates sub-word tokenizers for our dataset"""
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
+            "neuralmind/bert-base-portuguese-cased")
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            "bert-base-uncased")
 
+        def iterate_pt():
+            """Generate Portuguese sentences one at a time from the dataset"""
+            for pt, _ in data:
+                yield pt.numpy().decode('utf-8')
+
+        def iterate_en():
+            """Generate English sentences one at a time from the dataset"""
+            for _, en in data:
+                yield en.numpy().decode('utf-8')
+
+        tokenizer_pt = tokenizer_pt.train_new_from_iterator(
+            iterate_pt(), vocab_size=2**13)
+        tokenizer_en = tokenizer_en.train_new_from_iterator(
+            iterate_en(), vocab_size=2**13)
+        return tokenizer_pt, tokenizer_en
     def encode(self, pt, en):
-        """Encodes a translation pair into tokens"""
-        pt_str = pt.numpy().decode('utf-8')
-        en_str = en.numpy().decode('utf-8')
-        pt_tokens = self.tokenizer_pt.encode(pt_str)
-        en_tokens = self.tokenizer_en.encode(en_str)
-        pt_tokens = [self.pt_vocab_size] + pt_tokens + [self.pt_vocab_size + 1]
-        en_tokens = [self.en_vocab_size] + en_tokens + [self.en_vocab_size + 1]
-        return np.array(pt_tokens), np.array(en_tokens)
+        """
+        Encodes a translation into tokens
+        """
+        pt_tokens = [self.tokenizer_pt.vocab_size] + \
+            self.tokenizer_pt.encode(pt.numpy().decode('utf-8')) + \
+            [self.tokenizer_pt.vocab_size + 1]
+        en_tokens = [self.tokenizer_en.vocab_size] + \
+            self.tokenizer_en.encode(en.numpy().decode('utf-8')) + \
+            [self.tokenizer_en.vocab_size + 1]
+        return pt_tokens, en_tokens
