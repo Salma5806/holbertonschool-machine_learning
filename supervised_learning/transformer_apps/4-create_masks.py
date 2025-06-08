4941c463baf85295a8a3cae56791104559cb7ca7
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-08 21:58:16 +0100

    Update 4-create_masks.py

--- supervised_learning/transformer_apps/4-create_masks.py
+++ supervised_learning/transformer_apps/4-create_masks.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
-
-
+"""
+function that creates masks for training/validation
+"""
 import tensorflow as tf
 
+
+def create_padding_mask(seq):
+    """
+    creates a mask to be applied for a given sequence
+    """
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    """(batch_size, 1, 1, seq_len)"""
+    return seq[:, tf.newaxis, tf.newaxis, :]
+
+
+def create_look_ahead_mask(size):
+    """creates a mask"""
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    """(seq_len, seq_len)"""
+    return mask
+
+
 def create_masks(inputs, target):
-    """Generate attention masks for the Transformer model"""
-
-    def padding_mask(seq):
-    	""" """
-        mask = tf.cast(seq == 0, tf.float32)
-        return mask[:, tf.newaxis, tf.newaxis, :]
-
-    def look_ahead_mask(length):
-        """ """
-        return 1 - tf.linalg.band_part(tf.ones((length, length)), -1, 0)
-
-    encoder_mask = padding_mask(inputs)
-
-    decoder_pad_mask = padding_mask(target)
-    future_mask = look_ahead_mask(tf.shape(target)[1])
-    combined_mask = tf.maximum(decoder_pad_mask[:, :, :, :tf.shape(target)[1]],
-                               future_mask[tf.newaxis, tf.newaxis, :, :])
-
-    decoder_mask = encoder_mask
-
-    return encoder_mask, combined_mask, decoder_mask
-
+    """encoder and decoder masks and look ahead mask"""
+    encoder_mask = create_padding_mask(inputs)
+    decoder_padding_mask = create_padding_mask(inputs)
+    look_ahead_mask = create_look_ahead_mask(tf.shape(target)[1])
+    dec_target_padding_mask = create_padding_mask(target)
+    combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+    return encoder_mask, combined_mask, decoder_padding_mask
