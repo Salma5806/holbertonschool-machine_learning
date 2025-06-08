f72df4bb67dd60b10ff4ae23c4a95c6789a30ce0
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-08 21:40:11 +0100

    Update 2-dataset.py

--- supervised_learning/transformer_apps/2-dataset.py
+++ supervised_learning/transformer_apps/2-dataset.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""task 0"""
+"""task 2"""
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
@@ -13,15 +13,28 @@ class Dataset:
     """
 
     def __init__(self):
-        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='train',
-                                    as_supervised=True)
-        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='validation',
-                                    as_supervised=True)
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
+        """
+        Class constructor
+        """
+        # Chargement du dataset brut
+        raw_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                              split='train',
+                              as_supervised=True)
+        raw_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                              split='validation',
+                              as_supervised=True)
+
+        # Création des tokenizers
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(raw_train)
+
+        # Tokenisation et encodage des datasets (avec tf_encode)
+        self.data_train = raw_train.map(self.tf_encode,
+                                       num_parallel_calls=tf.data.AUTOTUNE)
+        self.data_valid = raw_valid.map(self.tf_encode,
+                                       num_parallel_calls=tf.data.AUTOTUNE)
 
     def tokenize_dataset(self, data):
+        """Creates sub-word tokenizers for our dataset"""
         tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
             "neuralmind/bert-base-portuguese-cased")
         tokenizer_en = transformers.AutoTokenizer.from_pretrained(
@@ -35,13 +48,18 @@ class Dataset:
             for _, en in data:
                 yield en.numpy().decode('utf-8')
 
+        # Entraînement des tokenizers sur les données (vocab_size = 8192)
         tokenizer_pt = tokenizer_pt.train_new_from_iterator(
             iterate_pt(), vocab_size=2**13)
         tokenizer_en = tokenizer_en.train_new_from_iterator(
             iterate_en(), vocab_size=2**13)
+
         return tokenizer_pt, tokenizer_en
 
     def encode(self, pt, en):
+        """
+        Encodes a translation into tokens
+        """
         pt_tokens = [self.tokenizer_pt.vocab_size] + \
             [int(t) for t in self.tokenizer_pt.encode(pt.numpy().decode('utf-8'),
                                                      add_special_tokens=False)] + \
@@ -53,6 +71,9 @@ class Dataset:
         return pt_tokens, en_tokens
 
     def tf_encode(self, pt, en):
+        """
+        Acts as a TensorFlow wrapper for the encode instance method
+        """
         pt, en = tf.py_function(func=self.encode,
                                 inp=[pt, en],
                                 Tout=[tf.int64, tf.int64])
