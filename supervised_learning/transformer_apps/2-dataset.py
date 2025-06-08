5136f6b900fcc3d0c7964d59cb2d07b0ff941ba6
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-06-08 21:37:37 +0100

    Update 2-dataset.py

--- supervised_learning/transformer_apps/2-dataset.py
+++ supervised_learning/transformer_apps/2-dataset.py
@@ -13,33 +13,25 @@ class Dataset:
     """
 
     def __init__(self):
-        """
-        Class constructor
-        """
         self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
                                     split='train',
                                     as_supervised=True)
         self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
                                     split='validation',
                                     as_supervised=True)
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train
-        )
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
 
     def tokenize_dataset(self, data):
-        """Creates sub-word tokenizers for our dataset"""
         tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
             "neuralmind/bert-base-portuguese-cased")
         tokenizer_en = transformers.AutoTokenizer.from_pretrained(
             "bert-base-uncased")
 
         def iterate_pt():
-            """Generate Portuguese sentences one at a time from the dataset"""
             for pt, _ in data:
                 yield pt.numpy().decode('utf-8')
 
         def iterate_en():
-            """Generate English sentences one at a time from the dataset"""
             for _, en in data:
                 yield en.numpy().decode('utf-8')
 
@@ -48,24 +40,19 @@ class Dataset:
         tokenizer_en = tokenizer_en.train_new_from_iterator(
             iterate_en(), vocab_size=2**13)
         return tokenizer_pt, tokenizer_en
+
     def encode(self, pt, en):
-        """
-        Encodes a translation into tokens
-        """
         pt_tokens = [self.tokenizer_pt.vocab_size] + \
-            self.tokenizer_pt.encode(pt.numpy().decode('utf-8'),
-                                     add_special_tokens=False) + \
+            [int(t) for t in self.tokenizer_pt.encode(pt.numpy().decode('utf-8'),
+                                                     add_special_tokens=False)] + \
             [self.tokenizer_pt.vocab_size + 1]
         en_tokens = [self.tokenizer_en.vocab_size] + \
-            self.tokenizer_en.encode(en.numpy().decode('utf-8'),
-                                     add_special_tokens=False) + \
+            [int(t) for t in self.tokenizer_en.encode(en.numpy().decode('utf-8'),
+                                                     add_special_tokens=False)] + \
             [self.tokenizer_en.vocab_size + 1]
         return pt_tokens, en_tokens
 
     def tf_encode(self, pt, en):
-        """
-        Acts as a TensorFlow wrapper for the encode instance method
-        """
         pt, en = tf.py_function(func=self.encode,
                                 inp=[pt, en],
                                 Tout=[tf.int64, tf.int64])
