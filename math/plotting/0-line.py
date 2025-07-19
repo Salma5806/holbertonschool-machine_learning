256fe104b0d7fa4400628b4efb02723586f5e2dc
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-19 21:36:58 +0100

    Update 0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -3,6 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
-plt.xlim(0, 10)
-plt.plot(y, color='red')
+x = np.arange(0, 11)
+plt.plot(x, y, color='red')
 plt.show()
