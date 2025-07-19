59a731b447ff413f8cc0e44a2f8844eb4f64bdc4
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-19 21:46:29 +0100

    Update 1-scatter.py

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -2,13 +2,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-mean = [69, 0]
-cov = [[15, 8], [8, 15]]
-np.random.seed(5)
-x, y = np.random.multivariate_normal(mean, cov, 2000).T
-y += 180
-plt.title('Men''s Height vs Weight')
-plt.xlabel('Height (in)')
-plt.ylabel('Weight (lbs)')
-plt.scatter(x, y, color="mediumorchid", label= 'Data Points')
-plt.savefig("my1graph.png")
+def scatter():
+    """
+    scatter plot
+    """
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+    plt.scatter(x, y, color='magenta')  # magenta points
+    plt.xlabel('Height (in)')           # x-axis label
+    plt.ylabel('Weight (lbs)')          # y-axis label
+    plt.title("Men's Height vs Weight") # title
+    plt.show()
