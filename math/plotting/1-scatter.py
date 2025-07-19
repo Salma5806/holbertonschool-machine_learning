9eb121edba36f004ecd7bb37df0c712e8c1d2d04
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-19 21:48:39 +0100

    Update 1-scatter.py

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -2,18 +2,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def scatter():
-    """
-    scatter plot
-    """
+    """Generate a scatter plot of men's height vs weight."""
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
     plt.figure(figsize=(6.4, 4.8))
-    plt.scatter(x, y, color='magenta')  # magenta points
-    plt.xlabel('Height (in)')           # x-axis label
-    plt.ylabel('Weight (lbs)')          # y-axis label
-    plt.title("Men's Height vs Weight") # title
+    plt.scatter(x, y, color='magenta')
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title("Men's Height vs Weight")
     plt.show()
