2659207d6755ce23d99495743cf97f4bd1dacdbf
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-19 22:31:48 +0100

    Update 4-frequency.py

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python3
+"""4. Frequency"""
 import numpy as np
 import matplotlib.pyplot as plt
-"""4. Frequency
-This script plots a histogram of student grades for Project A.
-"""
 
 
 def frequency():
-    """Plot a histogram of student grades with specified formatting."""
+    """
+    plot a histogram of student scores for a project
+    """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-    
-    # Clip grades so no values outside 0-100 bins
-    student_grades = np.clip(student_grades, 0, 100)
-    
     plt.figure(figsize=(6.4, 4.8))
-    
-    # bins edges from 0 to 100 every 10 units, add a small epsilon to include 100 exactly
-    bins = np.arange(0, 101, 10)
-    
-    plt.hist(student_grades, bins=bins, edgecolor='black', density=False)
-    plt.xlabel('Grades')
-    plt.ylabel('Number of Students')
     plt.title('Project A')
+    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='black')
+    plt.xlabel('Grades')
     plt.xlim(0, 100)
+    plt.ylabel('Number of Students')
+    plt.ylim(0, 30)
+    plt.xticks(np.arange(0, 101, 10))
     plt.show()
