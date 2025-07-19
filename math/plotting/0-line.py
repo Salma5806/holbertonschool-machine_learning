7145c09fc5e220b00b9007c5c7b1696156a20831
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-19 21:40:12 +0100

    Update 0-line.py

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+"""0. Line Graph"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-y = np.arange(0, 11) ** 3
-x = np.arange(0, 11)
-plt.plot(x, y, color='red')
-plt.show()
+
+def line():
+    """plot y as a line graph"""
+    y = np.arange(0, 11) ** 3
+    print(y)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(y, color='red')
+    plt.xlim(0, 10)
+    plt.show()
