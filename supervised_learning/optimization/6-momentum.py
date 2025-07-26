2b0fb06afaee2012421802903a2acbb152cf7e47
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-26 18:47:05 +0100

    Update 6-momentum.py

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python3
-"""  creates the training operation for a neural network
- in tensorflow using the gradient descent with momentum
- optimization algorithm
- """
+"""creates the training operation for a neural network in
+tensorflow using the gradient descent with momentum
+optimization algorithm"""
 
-import tensorflow.compat.v1 as tf
+
+import tensorflow as tf
 
 
 def create_momentum_op(loss, alpha, beta1):
-    """
-    Creates the training operation for a neural network in TensorFlow
-    using the gradient descent with momentum optimization algorithm.
+    """creates the training operation for a neural network in
+tensorflow using the gradient descent with momentum
+optimization algorithm"""
 
-    Args:
-        loss: The loss of the network.
-        alpha: The learning rate.
-        beta1: The momentum weight.
-
-    Returns:
-        The momentum optimization operation.
-    """
     optimizer = tf.train.MomentumOptimizer(learning_rate=alpha, momentum=beta1)
-    train_op = optimizer.minimize(loss)
-
-    return train_op
+    
+    return optimizer.minimize(loss)
