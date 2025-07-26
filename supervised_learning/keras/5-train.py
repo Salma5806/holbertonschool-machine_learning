d5e589d2534c169042a0786c174add49a12791b3
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-26 18:18:56 +0100

    Update 5-train.py

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -1,25 +1,18 @@
 #!/usr/bin/env python3
-""" trains a model """
-
+"""
+using the validation
+argument
+"""
 import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
     """
-    Trains a model using mini-batch gradient
-      descent and optionally validates it.
+    adding the validation data to
+    the training process
     """
-    if validation_data:
-        val_data, val_labels = validation_data
-        validation_data = (val_data, val_labels)
-    history = network.fit(
-        x=data,
-        y=labels,
-        batch_size=batch_size,
-        epochs=epochs,
-        verbose=verbose,
-        shuffle=shuffle,
-        validation_data=validation_data
-    )
-    return history
+    trained = network.fit(x=data, y=labels, batch_size=batch_size,
+                          epochs=epochs, verbose=verbose, shuffle=shuffle,
+                          validation_data=validation_data)
+    return trained
