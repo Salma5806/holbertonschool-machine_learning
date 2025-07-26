dfc8e12e13ca5920e2fa71643dc17faf717f72d2
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-26 19:07:09 +0100

    Update 10-Adam.py

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
-"""  creates the training operation for a neural network in
-tensorflow using the Adam optimization algorithm """
+"""
+Function to create an Adam optimizer in TensorFlow
+"""
 
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
-def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+def create_Adam_op(alpha, beta1, beta2, epsilon):
     """
-    Creates the training operation for a neural network
-      in TensorFlow using the Adam optimization algorithm.
+    Sets up the Adam optimization algorithm in TensorFlow.
 
     Args:
-        loss: The loss of the network.
-        alpha: The learning rate.
-        beta1: The weight used for the first moment.
-        beta2: The weight used for the second moment.
-        epsilon: A small number to avoid division by zero.
+        alpha (float): learning rate
+        beta1 (float): weight for the first moment estimate
+        beta2 (float): weight for the second moment estimate
+        epsilon (float): small constant to prevent division by zero
+
     Returns:
-        The Adam optimization operation.
+        optimizer (tf.keras.optimizers.Optimizer): Adam optimizer
     """
-    return tf.train.AdamOptimizer(learning_rate=alpha,
-                                  beta1=beta1, beta2=beta2,
-                                  epsilon=epsilon).minimize(loss)
+    return tf.keras.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
+        epsilon=epsilon
+    )
