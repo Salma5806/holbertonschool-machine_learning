0c94b5e348113e8cb4d99d6a8967d80202c22d08
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-26 19:14:32 +0100

    Update 14-batch_norm.py

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
-""" creates a batch normalization layer for a neural network in tensorflow """
+"""
+Creates a batch normalization layer for a neural network in TensorFlow.
+"""
 
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 
 def create_batch_norm_layer(prev, n, activation):
     """
-    Create a batch normalization layer for a neural network in TensorFlow.
+    Creates a batch normalization layer.
 
     Args:
-        prev: Activated output of the previous layer.
-        n: Number of nodes in the layer to be created.
-        activation: Activation function to be used.
+        prev: activated output of the previous layer
+        n: number of nodes in the layer to be created
+        activation: activation function for the output
 
     Returns:
-        A tensor of the activated output for the layer.
+        A tensor of the activated output for the layer
     """
-    kernel_initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer = tf.keras.layers.Dense(units=n,
-                                  kernel_initializer=kernel_initializer,
-                                  activation=None)(prev)
-    gamma = tf.Variable(initial_value=tf.ones((n,)), trainable=True)
-    beta = tf.Variable(initial_value=tf.zeros((n,)), trainable=True)
-    epsilon = 1e-8
-    mean, variance = tf.nn.moments(layer, axes=[0])
-    normalized = tf.nn.batch_normalization(layer, mean, variance,
-                                           beta, gamma, epsilon)
+    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    dense = tf.keras.layers.Dense(units=n, kernel_initializer=init)
+    z = dense(prev)
+    mean, variance = tf.nn.moments(z, axes=[0])
+    gamma = tf.Variable(tf.ones([n]), trainable=True)
+    beta = tf.Variable(tf.zeros([n]), trainable=True)
+    epsilon = 1e-7
+    normalized = tf.nn.batch_normalization(z, mean, variance, beta, gamma, epsilon)
     return activation(normalized)
