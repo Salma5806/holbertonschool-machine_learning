07ddded8719853702dc17109eefd9e8d275a1729
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-26 18:17:46 +0100

    Update 4-train.py

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-""" trains a model using mini batch """
+"""
+training a keras model
+in any needed case following certain variables
+"""
 
 import tensorflow.keras as K
 
@@ -7,12 +10,8 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size,
                 epochs, verbose=True, shuffle=False):
     """
-    Trains a model using mini-batch gradient descent."""
-    history = network.fit(
-        x=data,
-        y=labels,
-        batch_size=batch_size,
-        epochs=epochs,
-        verbose=verbose,
-        shuffle=shuffle)
-    return history
+    Trains the given network with the provided data and labels
+    """
+    trained = network.fit(x=data, y=labels, batch_size=batch_size,
+                          epochs=epochs, verbose=verbose, shuffle=shuffle)
+    return trained
