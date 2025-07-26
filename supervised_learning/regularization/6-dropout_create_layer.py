16f3d937e9d59276d027d7a3164f4c77b41d2d4a
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-26 21:12:48 +0100

    Update 6-dropout_create_layer.py

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-"""creates a layer of a neural network using dropout"""
-
-
+""" Task 6"""
 import tensorflow as tf
 
 
-def dropout_create_layer(prev, n, activation, keep_prob):
-    """creates a layer of a neural network using dropout"""
-    initializer = tf.contrib.layers.\
-        variance_scaling_initializer(mode="FAN_AVG")
-    dropout = tf.layers.Dropout(rate=(1-keep_prob))
-    layer = tf.layers.Dense(units=n, activation=activation,
-                            kernel_initializer=initializer,
-                            kernel_regularizer=dropout)
-    return (layer(prev))
+def dropout_create_layer(prev, n, activation, keep_prob, training=True):
+    """Creates a dense layer with dropout regularization"""
+
+    initializer = tf.keras.initializers.VarianceScaling(
+        scale=2.0, mode='fan_avg')
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=initializer)(prev)
+
+    if training:
+        layer = tf.nn.dropout(layer, rate=1 - keep_prob)
+
+    return layer
