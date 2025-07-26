0b921e6b434fcd592c674bafb9aadd74b383913e
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-26 18:16:47 +0100

    Update 2-optimize.py

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
-""" optimization model """
+"""
+using the adam optimizer
+for the already built model
+"""
 
 import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
     """
-    Sets up Adam optimization for a Keras model with
-      categorical crossentropy loss and accuracy metrics.
+    optimizing the network by using
+    adam optimizer with the given parameters
+    alpha as learning rate, beta1 as the first
+    optimization parameter and beta2 as the second
     """
-    optimizer = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
-    network.compile(optimizer=optimizer,
-                    loss='categorical_crossentropy', metrics=['accuracy'])
+    opt = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(optimizer=opt,
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+    return None
