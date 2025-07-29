02a2f46e54e8e91540ebff6b16408e8f7e079693
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-29 21:14:09 +0100

    Update 4-convolve_channels.py

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -6,22 +6,38 @@ import numpy as np
 def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """cnvolve_grayscale"""
     m, h, w, c = images.shape
-    kh, kw, _, nc = kernel.shape
+    kh, kw, _ = kernel.shape
+
     sh, sw = stride
-    if padding == 'valid':
-        pad_h = 0
-        pad_w = 0
-    elif padding == 'same':
-        pad_h = int(((h - 1) * sh + kh - h) / 2) + 1
-        pad_w = int(((w - 1) * sw + kw - w) / 2) + 1
-    elif isinstance(padding, tuple) and len(padding) == 2:
-        pad_h, pad_w = padding
-    padded_images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), mode='constant', constant_values=0)
-    output_h = int((h + 2 * pad_h - kh) / sh) + 1
-    output_w = int((w + 2 * pad_w - kw) / sw) + 1
-    result = np.zeros((m, output_h, output_w, nc))
-    for i in range(output_h):
-        for j in range(output_w):
-            for k in range(nc):
-                result[:, i, j, k] = np.sum(padded_images[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :] * kernel[:, :, :, k])
-    return result
+
+    if padding == "valid":
+        ph, pw = 0, 0
+    elif padding == "same":
+        ph = int((((h - 1) * sh + kh - h) / 2) + 1)
+        pw = int((((w - 1) * sw + kw - w) / 2) + 1)
+
+    else:
+        ph, pw = padding
+    oh = int((h - kh + 2 * ph) / sh + 1)
+    ow = int((w - kw + 2 * pw) / sw + 1)
+    output = np.zeros(shape=(m, oh, ow))
+    images_padded = np.pad(
+        images,
+        [
+            (0, 0),
+            (ph, ph),
+            (pw, pw),
+            (0, 0)
+        ],
+        mode="constant"
+    )
+
+    for x in range(oh):
+        for y in range(ow):
+            output[:, x, y] = np.sum(
+                (kernel * images_padded[:, x *
+                 sh:x * sh + kh, y * sw:y * sw + kw]),
+                axis=(1, 2, 3)
+            )
+
+    return output
