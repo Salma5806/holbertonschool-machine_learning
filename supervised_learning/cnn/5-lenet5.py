26a241b25e045955d8f22011bca41c4d0bbb1e10
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-29 21:36:39 +0100

    Update 5-lenet5.py

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -1,29 +1,54 @@
 #!/usr/bin/env python3
-"""
-Builds a modified version of the LeNet-5 architecture using tensorflow
-"""
-import tensorflow.keras as K
+"""Builds a modified LeNet-5 model using Keras"""
+from tensorflow import keras as K
 
 
 def lenet5(X):
-    """
-    a function that builds a modified LeNet-5 using tensorflow"""
-    init = K.initializers.he_normal(seed=None)
-    conv1 = K.layers.Conv2D(filters=6, kernel_size=5, padding='same',
-                            activation="relu", kernel_initializer=init)(X)
-    pool1 = K.layers.MaxPooling2D(pool_size=[2, 2], strides=2)(conv1)
-    conv2 = K.layers.Conv2D(filters=16, kernel_size=5, padding='valid',
-                            activation="relu", kernel_initializer=init)(pool1)
-    pool2 = K.layers.MaxPooling2D(pool_size=[2, 2], strides=2)(conv2)
+    """Builds a modified LeNet-5 architect"""
+    he_init = K.initializers.HeNormal(seed=0)
+    conv1 = K.layers.Conv2D(
+        filters=6,
+        kernel_size=(5, 5),
+        padding='same',
+        activation='relu',
+        kernel_initializer=he_init
+    )(X)
+    pool1 = K.layers.MaxPooling2D(
+        pool_size=(2, 2),
+        strides=(2, 2)
+    )(conv1)
+    conv2 = K.layers.Conv2D(
+        filters=16,
+        kernel_size=(5, 5),
+        padding='valid',
+        activation='relu',
+        kernel_initializer=he_init
+    )(pool1)
+    pool2 = K.layers.MaxPooling2D(
+        pool_size=(2, 2),
+        strides=(2, 2)
+    )(conv2)
     flat = K.layers.Flatten()(pool2)
-    fc1 = K.layers.Dense(units=120, activation="relu",
-                         kernel_initializer=init)(flat)
-    fc2 = K.layers.Dense(units=84, activation="relu",
-                         kernel_initializer=init)(fc1)
-    y_pred = K.layers.Dense(units=10, activation="softmax",
-                            kernel_initializer=init)(fc2)
-    model = K.models.Model(inputs=X, outputs=y_pred)
-    model.compile(optimizer=K.optimizers.Adam(),
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    fc1 = K.layers.Dense(
+        units=120,
+        activation='relu',
+        kernel_initializer=he_init
+    )(flat)
+    fc2 = K.layers.Dense(
+        units=84,
+        activation='relu',
+        kernel_initializer=he_init
+    )(fc1)
+    output = K.layers.Dense(
+        units=10,
+        activation='softmax',
+        kernel_initializer=he_init
+    )(fc2)
+    model = K.Model(inputs=X, outputs=output)
+    model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
     return model
