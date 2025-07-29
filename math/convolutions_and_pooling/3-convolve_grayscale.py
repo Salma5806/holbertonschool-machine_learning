1506adfc77b6beec181b27ed3de3047b8a8248f6
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-07-29 21:07:24 +0100

    Update 3-convolve_grayscale.py

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -4,10 +4,11 @@ import numpy as np
 
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
-    """cnvolve_grayscale"""
+    """convolve_grayscale"""
     m, h, w = images.shape
     kh, kw = kernel.shape
     sh, sw = stride
+
     if padding == 'valid':
         pad_h, pad_w = 0, 0
     elif padding == 'same':
@@ -17,15 +18,23 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
         pad_h, pad_w = padding
     else:
         raise ValueError("Invalid padding value")
-    padded_images = np.pad(images, ((0, 0),
-                                    (pad_h, pad_h),
-                                    (pad_w, pad_w)),
-                                    mode='constant',
-                                    constant_values=0)
+
+    padded_images = np.pad(
+        images,
+        ((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
+        mode='constant',
+        constant_values=0
+    )
+
     output_h = int((h + 2 * pad_h - kh) / sh) + 1
     output_w = int((w + 2 * pad_w - kw) / sw) + 1
     result = np.zeros((m, output_h, output_w))
+
     for i in range(output_h):
         for j in range(output_w):
-            result[:, i, j] = np.sum(padded_images[:, i*sh:i*sh+kh, j*sw:j*sw+kw] * kernel, axis=(1, 2))
+            result[:, i, j] = np.sum(
+                padded_images[:, i * sh:i * sh + kh, j * sw:j * sw + kw] * kernel,
+                axis=(1, 2)
+            )
+
     return result
