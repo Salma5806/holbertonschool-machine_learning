93d9a546229b1b407170c41b1f5e3b8fa8ffca2f
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-03 22:47:28 +0100

    Update 2-identity_block.py

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
 """ Identity Block """
-
-import tensorflow as tf
-from tensorflow.keras import layers, initializers
+from tensorflow import keras as K
 
 def identity_block(A_prev, filters):
-    """Builds an identity block as described in ResNet paper.
+    """
+    Builds an identity block as described in the paper:
+    Deep Residual Learning for Image Recognition (He et al.)
 
     Args:
-        A_prev: Output from the previous layer (tensor).
-        filters: Tuple/list of 3 integers [F11, F3, F12] representing the number
-                 of filters in each convolutional layer.
+        A_prev: output from the previous layer
+        filters: list or tuple of three integers [F11, F3, F12]
 
     Returns:
-        The activated output of the identity block.
+        Activated output of the identity block
     """
     F11, F3, F12 = filters
-    initializer = initializers.HeNormal(seed=0)
-    X = layers.Conv2D(filters=F11, kernel_size=(1, 1), padding='same',
-                      kernel_initializer=initializer)(A_prev)
-    X = layers.BatchNormalization(axis=3)(X)
-    X = layers.Activation('relu')(X)
-    X = layers.Conv2D(filters=F3, kernel_size=(3, 3), padding='same',
-                      kernel_initializer=initializer)(X)
-    X = layers.BatchNormalization(axis=3)(X)
-    X = layers.Activation('relu')(X)
-    X = layers.Conv2D(filters=F12, kernel_size=(1, 1), padding='same',
-                      kernel_initializer=initializer)(X)
-    X = layers.BatchNormalization(axis=3)(X)
-    X = layers.Add()([X, A_prev])
-    X = layers.Activation('relu')(X)
+    initializer = K.initializers.HeNormal()
+    X = K.layers.Conv2D(F11, (1, 1), padding='same',
+                        kernel_initializer=initializer)(A_prev)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+    X = K.layers.Conv2D(F3, (3, 3), padding='same',
+                        kernel_initializer=initializer)(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Activation('relu')(X)
+    X = K.layers.Conv2D(F12, (1, 1), padding='same',
+                        kernel_initializer=initializer)(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.Add()([X, A_prev])
+    X = K.layers.Activation('relu')(X)
     return X
