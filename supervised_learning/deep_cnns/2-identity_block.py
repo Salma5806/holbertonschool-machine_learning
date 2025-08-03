22bfe12eeaaa0105515ab5d7cb8d5a2b4682c339
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-03 22:17:44 +0100

    Update 2-identity_block.py

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -10,7 +10,7 @@ def identity_block(A_prev, filters):
 
     Parameters:
     - A_prev: output from the previous layer
-    - filters: tuple of (F11, F3, F12):
+    - filters: tuple of (F11, F3, F12)
         F11: filters for the first 1x1 conv
         F3: filters for the 3x3 conv
         F12: filters for the second 1x1 conv
@@ -19,24 +19,26 @@ def identity_block(A_prev, filters):
     - Output tensor for the block
     """
     weights_init = K.initializers.he_normal()
-
     F11, F3, F12 = filters
 
     layer1 = K.layers.Conv2D(
         filters=F11, kernel_size=(1, 1),
-        padding='same', kernel_initializer=weights_init)(A_prev)
+        padding='same', kernel_initializer=weights_init
+    )(A_prev)
     bn1 = K.layers.BatchNormalization(axis=3)(layer1)
     act1 = K.layers.Activation('relu')(bn1)
 
     layer2 = K.layers.Conv2D(
         filters=F3, kernel_size=(3, 3),
-        padding='same', kernel_initializer=weights_init)(act1)
+        padding='same', kernel_initializer=weights_init
+    )(act1)
     bn2 = K.layers.BatchNormalization(axis=3)(layer2)
     act2 = K.layers.Activation('relu')(bn2)
 
     layer3 = K.layers.Conv2D(
         filters=F12, kernel_size=(1, 1),
-        padding='same', kernel_initializer=weights_init)(act2)
+        padding='same', kernel_initializer=weights_init
+    )(act2)
     bn3 = K.layers.BatchNormalization(axis=3)(layer3)
 
     added = K.layers.Add()([bn3, A_prev])
