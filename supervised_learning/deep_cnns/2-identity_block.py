37d687c3dbb7b5464e0bf9e53a993887827be4b3
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-03 22:48:42 +0100

    Update 2-identity_block.py

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -6,16 +6,17 @@ def identity_block(A_prev, filters):
     """
     Builds an identity block as described in the paper:
     Deep Residual Learning for Image Recognition (He et al.)
-
+    
     Args:
         A_prev: output from the previous layer
         filters: list or tuple of three integers [F11, F3, F12]
-
+        
     Returns:
         Activated output of the identity block
     """
     F11, F3, F12 = filters
     initializer = K.initializers.HeNormal()
+
     X = K.layers.Conv2D(F11, (1, 1), padding='same',
                         kernel_initializer=initializer)(A_prev)
     X = K.layers.BatchNormalization(axis=3)(X)
