ad3a5384b2a70ba18d61f3ae8e0d113ab16b7d41
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-10 19:56:57 +0100

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
-""" Get requests location from Github API"""
-import sys
+"""Program By using the GitHub API, write a script that
+prints the location of a specific user"""
 import requests
+import sys
 import time
 
 
 if __name__ == '__main__':
-
-    url = sys.argv[1]
-    headers = {'Accept': 'application/vnd.github.v3+json'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        print(response.json()['location'])
-    if response.status_code == 404:
-        print('Not found')
-    if response.status_code == 403:
-        limit = int(response.headers['X-Ratelimit-Reset'])
-        start = int(time.time())
-        elapsed = int((limit - start) / 60)
-        print('Reset in {} min'.format(int(elapsed)))
+    data = requests.get(sys.argv[1])
+    response = data.status_code
+    if response == 200:
+        print(data.json()['location'])
+    elif response == 403:
+        reset = data.headers['X-Ratelimit-Reset']
+        reset = int(reset) - int(time.time())
+        print("Reset in {} min".format(int(reset / 60)))
+    else:
+        print("Not found")
