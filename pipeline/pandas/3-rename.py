369fcb412dd9a7d9e662f356ae6c6ccf1288a94b
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-10 19:47:28 +0100

    Update 3-rename.py

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
-"""
-task 3
-"""
-
+"""Program that created a pd.DataFrame from a file"""
 import pandas as pd
 
 
-def rename(df):
-    """
-    Takes a pd.DataFrame as input and performs the following:
-    """
-    df_modified = df.copy()
-    df_modified = df_modified.rename(columns={"Timestamp": "Datetime"})
-    df_modified["Datetime"] = pd.to_datetime(df_modified["Datetime"], unit="s")
-    return df_modified[["Datetime", "Close"]]
+def from_file(filename, delimiter):
+    """Reads a file and returns a dataframe"""
+    return pd.read_csv(filename, delimiter=delimiter)
