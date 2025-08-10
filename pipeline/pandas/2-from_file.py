700eff34f078c4e7f258b4a07d494acbd0ede7b0
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-10 19:49:13 +0100

    Update 2-from_file.py

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-
+"""task2"""
 import pandas as pd
 
+
 def from_file(filename, delimiter):
-    # Load data from file into DataFrame
-    df = pd.read_csv(filename, delimiter=delimiter)
-    return df
+    """Reads a file and returns a dataframe"""
+    return pd.read_csv(filename, delimiter=delimiter)
