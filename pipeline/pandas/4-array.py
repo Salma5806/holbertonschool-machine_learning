a25ce07becc5f11135959f1bb83b3940876e2b37
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-10 19:52:40 +0100

    Update 4-array.py

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """
-task 4
+Module that contains a function to convert DataFrame columns to numpy array
 """
 
 
 def array(df):
-    """Takes a pd.DataFrame as input and performs the following"""
+    """
+    Takes a pd.DataFrame as input and performs the following:
+    """
+    last_10_rows = df[["High", "Close"]].tail(10)
     return last_10_rows.to_numpy()
