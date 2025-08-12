cc9ed443d2a6f623776faaf3c1ba9824b7ca94a3
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-12 22:05:48 +0100

    Update 1-yolo.py

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -1,86 +1,85 @@
 #!/usr/bin/env python3
+"""
+The building class
+for the yolov3 algorithm
+allowing it to make predictions and load the data
+"""
+
 import tensorflow.keras as K
 import numpy as np
 
+
 class Yolo:
-    """Yolo class"""
+    """
+    Yolo class to define the elements
+    and functions making the algorithm work
+    """
     model = None
     class_names = None
     class_t = None
     nms_t = None
     anchors = None
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """
-        model_path is the path to where a Darknet Keras model is stored
-        classes_path is the path to where the list of class names used for the Darknet model, listed in order of index, can be found
-        class_t is a float representing the box score threshold for the initial filtering step
-        nms_t is a float representing the IOU threshold for non-max suppression
+        initialize the attributes for the yolo
+        algorithm
         """
-        self.model = K.models.load_model(model_path, compile=False)
-        self.class_names = self._load_classes(classes_path)
+        with open(classes_path, 'r') as file:
+            classes_list = [line.strip() for line in file if line.strip()]
+        self.model = K.models.load_model(model_path)
+        self.class_names = classes_list
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
-    def _load_classes(self, classes_path):
-        with open(classes_path) as file:
-            class_names = file.read().splitlines()
-        return class_names
+
     def process_outputs(self, outputs, image_size):
         """
-        Process the outputs of the YOLO model.
-
-        Args:
-            outputs: List of numpy.ndarray containing the model outputs.
-            image_size: Tuple (image_height, image_width) representing the original image size.
-
-        Returns:
-            boxes: List of numpy.ndarray containing the processed bounding boxes.
-            box_confidences: List of numpy.ndarray containing the box confidences.
-            box_class_probs: List of numpy.ndarray containing the box class probabilities.
+        Using the method cited in the research paper:
+        https://pjreddie.com/media/files/papers/YOLOv3.pdf
+        to get the bounding boxes out of the output
+        of the yolo model
         """
-        boxes = []
-        box_confidences = []
-        box_class_probs = []
-
-        image_height, image_width = image_size
-
         def sigmoid(x):
-            """Sigmoid function."""
-            return 1 / (1 + np.exp(-x))
-
-        for i, output in enumerate(outputs):
-
-            output = output[0] 
-
-            grid_h, grid_w, anchor_boxes, _ = output.shape
-
-            tx = output[..., 0]
-            ty = output[..., 1]
-            tw = output[..., 2]
-            th = output[..., 3]
-            box_confidence = output[..., 4:5]
-            class_probs = output[..., 5:]
-
-            tx_sigmoid = sigmoid(tx)
-            ty_sigmoid = sigmoid(ty)
-            box_confidence_sigmoid = sigmoid(box_confidence)
-            class_probs_sigmoid = sigmoid(class_probs)
-
-            cx = np.tile(np.arange(grid_w).reshape(1, grid_w, 1), (grid_h, 1, anchor_boxes))
-            cy = np.tile(np.arange(grid_h).reshape(grid_h, 1, 1), (1, grid_w, anchor_boxes))
-            bx = (tx_sigmoid + cx) / grid_w
-            by = (ty_sigmoid + cy) / grid_h
-            bw = (self.anchors[i][:, 0] * np.exp(tw)) / self.model.input.shape[1]
-            bh = (self.anchors[i][:, 1] * np.exp(th)) / self.model.input.shape[2]
-            x1 = (bx - bw / 2) * image_width
-            y1 = (by - bh / 2) * image_height
-            x2 = (bx + bw / 2) * image_width
-            y2 = (by + bh / 2) * image_height
-
-            box = np.stack([x1, y1, x2, y2], axis=-1)
+            z = 1/(1 + np.exp(-x))
+            return z
+        boxes, box_confidences, box_class_probs = [], [], []
+        for index, output in enumerate(outputs):
+            grid_h = output.shape[0]
+            grid_w = output.shape[1]
+            box_confidences.append(sigmoid(output[..., 4:5]))
+            box_class_probs.append(sigmoid(output[:, :, :, 5:]))
+            box = np.zeros(output[:, :, :, :4].shape)
+            t_x = output[:, :, :, 0]
+            t_y = output[:, :, :, 1]
+            t_w = output[:, :, :, 2]
+            t_h = output[:, :, :, 3]
+            pw_total = self.anchors[:, :, 0]
+            pw = np.tile(pw_total[index], grid_w)
+            pw = pw.reshape(grid_w, 1, len(pw_total[index]))
+            ph_total = self.anchors[:, :, 1]
+            ph = np.tile(ph_total[index], grid_h)
+            ph = ph.reshape(grid_h, 1, len(ph_total[index]))
+            cx = np.tile(np.arange(grid_w), grid_h)
+            cx = cx.reshape(grid_w, grid_w, 1)
+            cy = np.tile(np.arange(grid_w), grid_h)
+            cy = cy.reshape(grid_h, grid_h).T
+            cy = cy.reshape(grid_h, grid_h, 1)
+            bx = (1 / (1 + np.exp(-t_x))) + cx
+            by = (1 / (1 + np.exp(-t_y))) + cy
+            bw = np.exp(t_w) * pw
+            bh = np.exp(t_h) * ph
+            bx = bx / grid_w
+            by = by / grid_h
+            bw = bw / self.model.input.shape[1].value
+            bh = bh / self.model.input.shape[2].value
+            x1 = (bx - (bw / 2)) * image_size[1]
+            y1 = (by - (bh / 2)) * image_size[0]
+            x2 = (bx + (bw / 2)) * image_size[1]
+            y2 = (by + (bh / 2)) * image_size[0]
+            box[:, :, :, 0] = x1
+            box[:, :, :, 1] = y1
+            box[:, :, :, 2] = x2
+            box[:, :, :, 3] = y2
             boxes.append(box)
-
-            box_confidences.append(box_confidence_sigmoid)
-            box_class_probs.append(class_probs_sigmoid)
-
         return boxes, box_confidences, box_class_probs
