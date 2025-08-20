38c3c82a15ab05d0c76f5b1562d18268d9bc78b4
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-20 22:44:14 +0100

    Update 4-fasttext.py

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,39 +1,21 @@
 #!/usr/bin/env python3
-"""Task 4"""
+"""4. FastText"""
 
-from gensim.models import FastText
+import gensim
 
 
-def fasttext_model(sentences, size=100, min_count=5,
-                   negative=5, window=5, cbow=True,
-                   iterations=5, seed=0, workers=1):
-    """
-    Creates and trains a Gensim FastText model.
-      - sentences: A list of sentences to be trained on.
-      - size: The dimensionality of the embedding layer.
-      - min_count: The minimum number of occurrences of
-        a word for use in training.
-      - window: The maximum distance between the current
-        and predicted word within a sentence.
-      - negative: The size of negative sampling.
-      - cbow: A boolean to determine the training type;
-        True for CBOW, False for Skip-gram.
-      - iterations: The number of iterations to train over.
-      - seed: The seed for the random number generator.
-      - workers: The number of worker threads to train the model.
-      - Returns the trained FastText model.
-    """
-
+def fasttext_model(sentences, vector_size=100, min_count=5, window=5,
+                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Creates, builds and trains a gensim fastText model"""
     sg = 0 if cbow else 1
-
-    model = FastText(
-        sentences,
-        vector_size=size,
+    model = gensim.models.FastText(
+        sentences=sentences,
+        vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
         sg=sg,
-        epochs=iterations,
+        epochs=epochs,
         seed=seed,
         workers=workers
     )
