70cccc0f286264c4dc4a5d57daf43131a0a37d0c
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-20 21:20:52 +0100

    Update 0-bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,41 +1,13 @@
 #!/usr/bin/env python3
-"""Task 0"""
-
-import numpy as np
-import re
+""" Task 0"""
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
-    """
-    Creates a bag of words embedding matrix.
-      - sentences (list): A list of sentences to analyze.
-      - vocab (list, optional): A list of the vocabulary words to
-        use for the analysis.
-        If None, all words within sentences should be used.
-      - embeddings (numpy.ndarray): A 2D array of shape (s, f)
-        containing the embeddings, where s is the number of sentences
-        and f is the number of features.
-      - vocab (list): A list of the features used for embeddings.
-      - Tokenize the sentences into words, ignoring punctuation
-        (except in numbers) and converting to lowercase.
-      - If no vocabulary is provided, create one from the unique
-        words in the sentences.
-      - Create an empty matrix for the embeddings.
-      - Fill in the matrix with the word counts.
-    """
+    """Converts a list of sentences into a bag-of-words embedding matrix"""
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    x = vectorizer.fit_transform(sentences)
+    embedding = x.toarray()
+    features = vectorizer.get_feature_names_out()
 
-    words_list = [re.findall(r'\b\w[\w]*\b', re.sub(
-      r"'s\b", '', sentence.lower())) for sentence in sentences]
-
-    if vocab is None:
-        vocab = sorted(set(word for words in words_list for word in words))
-
-    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
-
-    for i, words in enumerate(words_list):
-        for word in words:
-            if word in vocab:
-                j = vocab.index(word)
-                embeddings[i, j] += 1
-
-    return embeddings, vocab
+    return embedding, features
