4e56fd0310c66ccd1b04e9db7ed5ef9638dacbee
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-20 22:32:42 +0100

    Update 3-gensim_to_keras.py

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python3
-"""Task 3"""
+"""task3"""
 
-from tensorflow.keras.layers import Embedding
-import numpy as np
+import tensorflow as tf 
 
 
 def gensim_to_keras(model):
-    """
-    Converts a trained Gensim Word2Vec model to a
-    trainable Keras Embedding layer.
-      - Get the vocabulary size and embedding dimensions
-        from the Gensim model
-      - Create an Embedding layer in Keras
-    """
-
-    vocab_size, embedding_dim = model.wv.vectors.shape
-
-    keras_embedding_layer = Embedding(
+    """Converts a gensim word2vec model to a keras Embedding layer"""
+    vocab_size = len(model.wv)
+    embedding_dim = model.vector_size
+    embedding_layer = tf.keras.layers.Embedding(
         input_dim=vocab_size,
         output_dim=embedding_dim,
         weights=[model.wv.vectors],
-        input_length=1,
         trainable=True
     )
 
-    return keras_embedding_layer
+    return embedding_layer
