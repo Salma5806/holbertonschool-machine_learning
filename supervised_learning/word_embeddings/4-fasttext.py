5b3732f1528ce1b0c6492355454de63d7807265f
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-20 22:51:31 +0100

    Update 4-fasttext.py

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,34 +1,23 @@
 #!/usr/bin/env python3
-"""task4"""
+"""Task4"""
+
 import gensim
 
 
 def fasttext_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """
-    Creates, builds and trains a gensim FastText model"""
-    try:
-        __import__("random").seed(seed)
-    except Exception:
-        pass
-    try:
-        __import__("numpy").random.seed(seed)
-    except Exception:
-        pass
+    """Creates, builds and trains a gensim fastText model"""
     sg = 0 if cbow else 1
     model = gensim.models.FastText(
+        sentences=sentences,
         vector_size=vector_size,
-        window=window,
         min_count=min_count,
+        window=window,
         negative=negative,
         sg=sg,
-        seed=seed,
-        workers=workers,
-    )
-    model.build_vocab(sentences)
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
         epochs=epochs,
+        seed=seed,
+        workers=workers
     )
+
     return model
