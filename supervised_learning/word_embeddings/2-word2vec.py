3ecce5b3a19e775077c60cd29e97b91ccc9ccc70
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-20 21:51:29 +0100

    Update 2-word2vec.py

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
-"""Task 2"""
+""" Task 2"""
 import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """Trains a Word2Vec model on the given sentences"""
+    try:
+        __import__("random").seed(seed)
+    except Exception:
+        pass
+
+    try:
+        __import__("numpy").random.seed(seed)
+    except Exception:
+        pass
     sg = 0 if cbow else 1
     model = gensim.models.Word2Vec(
         vector_size=vector_size,
