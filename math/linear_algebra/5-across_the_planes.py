0781c2c64c66c99ebafb57dbb747388065768eff
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-08-21 21:46:52 +0100

    Update 5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-""" function to add two matrices element-wise """
+"""
+Task 5
+"""
 
 
 def add_matrices2D(mat1, mat2):
-    """ adding two matrices element-wise """
-    if len(mat1) != len(mat2) \
-        or any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)):
-        return None
-    else:
-        return [[i + j for i, j in zip(row1, row2)] \
-                for row1, row2 in zip(mat1, mat2)]
+    """Add two matrices element-wise"""
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return (None)
+    SommedMat = []
+    for i in range(len(mat1)):
+        SommedMat.append([])
+        for j in range(len(mat1[0])):
+            SommedMat[i].append(mat1[i][j] + mat2[i][j])
+    return SommedMat
