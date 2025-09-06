f6ebe4532b5a357616c33a11d831ced7461418b9
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-09-06 19:43:54 +0100

    Update 0-monte_carlo.py

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -7,13 +7,10 @@ import numpy as np
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 alpha=0.1, gamma=0.99):
-    """Performs the Monte Carlo algorithm for estimating state-value function."""
-    n_states = V.shape[0]
-
+    """Monte Carlo prediction of value function"""
     for _ in range(episodes):
         state, _ = env.reset()
         episode = []
-
         for _ in range(max_steps):
             action = policy(state)
             next_state, reward, terminated, truncated, _ = env.step(action)
@@ -23,11 +20,11 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 break
         G = 0
         visited = set()
-        for state, reward in reversed(episode):
-            G = reward + gamma * G
-            if state not in visited:
-                visited.add(state)
-                V[state] += alpha * (G - V[state])
+        for t in reversed(range(len(episode))):
+            s, r = episode[t]
+            G = r + gamma * G
+            if s not in visited:
+                visited.add(s)
+                V[s] += alpha * (G - V[s])
 
     return V
-
