b73a248e1e18ec4f6a4148dc2537d12f0d879a9e
Salma5806 <113902287+Salma5806@users.noreply.github.com>
2025-09-06 20:23:53 +0100

    Update 2-sarsa_lambtha.py

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -7,7 +7,8 @@ import numpy as np
 
 def epsilon_greedy(Q, state, epsilon):
     """
-    Select an action using epsilon-greedy policy"""
+    Select an action using epsilon-greedy policy
+    """
     if np.random.uniform(0, 1) > epsilon:
         return np.argmax(Q[state])
     else:
@@ -26,7 +27,8 @@ def sarsa_lambtha(
         min_epsilon=0.1,
         epsilon_decay=0.05):
     """
-    Performs SARSA(λ) with eligibility traces"""
+    Performs SARSA(λ) with eligibility traces
+    """
     initial_epsilon = epsilon
 
     for episode in range(episodes):
@@ -37,14 +39,18 @@ def sarsa_lambtha(
         for step in range(max_steps):
             new_state, reward, terminated, truncated, _ = env.step(action)
             new_action = epsilon_greedy(Q, new_state, epsilon)
-            delta = reward + gamma * Q[new_state, new_action] - Q[state, action]
+
+            delta = (reward + gamma * Q[new_state, new_action] - Q[state, action])
             eligibility_traces[state, action] += 1
             Q += alpha * delta * eligibility_traces
             eligibility_traces *= lambtha * gamma
+
             state, action = new_state, new_action
             if terminated or truncated:
                 break
 
-        epsilon = min_epsilon + (initial_epsilon - min_epsilon) * np.exp(-epsilon_decay * episode)
+        epsilon = (min_epsilon +
+                   (initial_epsilon - min_epsilon) *
+                   np.exp(-epsilon_decay * episode))
 
     return Q
