b45d56c90df8f5bc7bc9368f044e00a264ba3e0f
Salma5806 <5806@holbertonstudents.com>
2025-10-05 20:26:28 +0100

    m

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -1,28 +1,46 @@
 #!/usr/bin/env python3
-""" Get request from SpaceX"""
+"""
+This script displays the first SpaceX launch with the following information:
+<launch name> (<date>) <rocket name> - <launchpad name> (<launchpad locality>)
+"""
 
 import requests
 
-if __name__ == '__main__':
-    url = 'https://api.spacexdata.com/v4/launches/upcoming'
-    response = requests.get(url).json()
-    date = [x['date_unix'] for x in response]
-    idx = date.index(min(date))
-    launch = response[idx]
-    launch_name = launch['name']
-    date_l = launch['date_local']
-    rocket_id = launch['rocket']
-    rocket_url = "https://api.spacexdata.com/v4/rockets/{}".format(rocket_id)
-    rocket_name = requests.get(rocket_url).json()['name']
-    lpad_id = launch['launchpad']
-    lpad_url = "https://api.spacexdata.com/v4/launchpads/{}".\
-        format(lpad_id)
-    lpad_req = requests.get(lpad_url).json()
-    lpad_name = lpad_req['name']
-    lpad_loc = lpad_req['locality']
 
-    upcoming_launch = "{} ({}) {} - {} ({})".format(launch_name, date_l,
-                                                    rocket_name, lpad_name,
-                                                    lpad_loc)
+if __name__ == "__main__":
+    # Fetch all launches
+    launches_url = "https://api.spacexdata.com/v4/launches"
+    rockets_url = "https://api.spacexdata.com/v4/rockets"
+    launchpads_url = "https://api.spacexdata.com/v4/launchpads"
 
-    print(upcoming_launch)
+    launches = requests.get(launches_url).json()
+    rockets = requests.get(rockets_url).json()
+    launchpads = requests.get(launchpads_url).json()
+
+    # Sort launches by date_unix (ascending)
+    launches.sort(key=lambda x: x.get("date_unix", float("inf")))
+
+    # Get the first launch (earliest one)
+    first_launch = launches[0]
+
+    # Extract information
+    launch_name = first_launch.get("name")
+    launch_date = first_launch.get("date_local")
+    rocket_id = first_launch.get("rocket")
+    launchpad_id = first_launch.get("launchpad")
+
+    # Find rocket name
+    rocket_name = next(
+        (r.get("name") for r in rockets if r.get("id") == rocket_id), "Unknown"
+    )
+
+    # Find launchpad name and locality
+    launchpad_data = next(
+        (lp for lp in launchpads if lp.get("id") == launchpad_id), {}
+    )
+    launchpad_name = launchpad_data.get("name", "Unknown")
+    launchpad_locality = launchpad_data.get("locality", "Unknown")
+
+    # Print result in required format
+    print(f"{launch_name} ({launch_date}) {rocket_name} - "
+          f"{launchpad_name} ({launchpad_locality})")
